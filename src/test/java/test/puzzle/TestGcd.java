4bfa2938a3ccef556f203879220f651c6d644881
saka1029 <saka1029@gmail.com>
2020-06-25 09:33:19 +0900

    Rational追加

---
+++ src/test/java/test/puzzle/TestGcd.java
@@ -0,0 +1,23 @@
+package test.puzzle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import puzzle.Rational;
+
+class TestGcd {
+
+    @Test
+    void test() {
+        assertEquals(1, Rational.gcd(2, 3));
+        assertEquals(1, Rational.gcd(3, 2));
+        assertEquals(4, Rational.gcd(72, 52));
+        assertEquals(4, Rational.gcd(52, 72));
+        try {
+            assertEquals(4, Rational.gcd(0, -52));
+            fail();
+        } catch (IllegalArgumentException e) {}
+    }
+
+}

636a567afc9de3721a389cc12c8d1a06b61d86e6
saka1029 <saka1029@gmail.com>
2020-07-02 14:42:58 +0900

    螺旋行列を追加

---
+++ src/test/java/test/puzzle/Test螺旋行列.java
@@ -0,0 +1,80 @@
+package test.puzzle;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class Test螺旋行列 {
+
+    /**
+     * 螺旋正方行列を求めます。
+     *
+     * s = 6の時は外側の6x6の枠、4x4の枠、2x2の枠の順に値を埋めます。
+     * 最初のfor文はnxnの枠を作ります。
+     * <pre>
+     * 6  6  6  6  6  6
+     * 6  4  4  4  4  6
+     * 6  4  2  2  4  6
+     * 6  4  2  2  4  6
+     * 6  4  4  4  4  6
+     * 6  6  6  6  6  6
+     * </pre>
+     * 次のfor文はnxnの枠の1辺を作ります。(4回繰り返します)
+     * 各辺の長さは(n - 1)になります。
+     * <pre>
+     * 5  -  -  -  >  5
+     * ^  3  -  >  3  |
+     * |  ^  1  1  |  |
+     * |  |  1  1  v  |
+     * |  3  <  -  3  v
+     * 5  <  -  -  -  5
+     * </pre>
+     * 最後のfor文は辺の中の各点を作ります。
+     *
+     * nは埋める値です。0から始めて値を格納するごとにインクリメントします。
+     * dxとdyは値を埋める方向です。
+     * <br>
+     * <pre>
+     * t = dx, dx = dy, dy = -t は方向を右回りに90度回転させます。
+     * 座標を左回りにΘ回転させる式は
+     *
+     * x' = x * cos(Θ) - y * sin(Θ)
+     * y' = x * sin(Θ) + y * cos(Θ)
+     *
+     * ここでは右回りに90度回転させるので、
+     * Θ = -π / 2, sin(Θ) = -1, cos(Θ) = 0 となり
+     *
+     * x' = y
+     * y' = -x
+     *
+     * となります。
+     *
+     * <br>
+     * sが奇数の時は中央の値（最後の値）だけループの外で埋めてやる必要があります。
+     *
+     * @param s 螺旋行列の1辺の大きさを指定します。
+     * @return s行s列の螺旋行列を返します。
+     */
+    static int[][] spiralMatrix(int s) {
+        int[][] a = new int[s][s];
+        int n = 0;
+        for (int b = s - 1, c = 0, x = 0, y = 0, dx = 0, dy = 1; b > 0; b -= 2, x = y = ++c)
+            for (int j = 0, t = 0; j < 4; ++j, t = dx, dx = dy, dy = -t)  // Repeat 4 times while changing the direction 90 degrees.
+                for (int i = 0; i < b; ++i, x += dx, y += dy, ++n)
+                    a[x][y] = n;
+        if (s % 2 == 1)             // if s is odd
+            a[s / 2][s / 2] = n;    // fill the last element at the center
+        return a;
+    }
+
+    @Test
+    public void testSpiralMatrix() {
+        for (int s = 0; s < 10; ++s) {
+            int[][] a = spiralMatrix(s);
+            System.out.println("s=" + s);
+            for (int[] row : a)
+                System.out.println(Arrays.toString(row));
+            System.out.println();
+        }
+    }
+}
--- src/test/java/test/puzzle/Test騎士巡歴問題.java
+++ src/test/java/test/puzzle/Test騎士巡歴問題.java
@@ -1,17 +1,21 @@
 package test.puzzle;
 
+import static org.junit.Assert.*;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 class Test騎士巡歴問題 {
 
-    static final int[][] MOVES = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1},
-        {1, -2}, {-1, -2}, {-2, -1},};
+    static final int[][] MOVES = {
+        {-2, 1}, {-1, 2}, {1, 2}, {2, 1},
+        {2, -1}, {1, -2}, {-1, -2}, {-2, -1},};
 
     static class Cell {
 
@@ -35,20 +39,18 @@ class Test騎士巡歴問題 {
             this.order = order;
             this.previous = previous;
             for (Cell m : moves)
-                if (m.previous == null && m.moves.size() <= 2)
-                    return false;
-            for (Cell m : moves)
-                if (m.previous == null)
-                    m.moves.remove(this);
+                m.moves.remove(this);
+            moves.remove(previous);
             return true;
         }
 
         void unsetPrevious() {
             assert this.previous != null : "previous is null";
-            this.order = 0;
-            this.previous = null;
+            moves.add(this.previous);
             for (Cell m : moves)
                 m.moves.add(this);
+            this.order = 0;
+            this.previous = null;
         }
 
         static Cell[][] board(int height, int width) {
@@ -69,21 +71,21 @@ class Test騎士巡歴問題 {
                 }
             return board;
         }
-        
+
         @Override
         public String toString() {
             return "Cell(" + order + ": moves.size=" + moves.size() + ")";
         }
     }
-    
+
     int 騎士巡歴(int height, int width, int startRow, int startCol, Consumer<Cell[][]> found) {
         int numberOfCells = height * width;
         Cell[][] board = Cell.board(height, width);
         Cell startCell = board[startRow][startCol];
         return new Object() {
-            
+
             int count = 0;
-            
+
             void visit(int i, Cell previous, Cell current) {
                 if (i == numberOfCells + 1 && current == startCell) {
                     ++count;
@@ -96,7 +98,8 @@ class Test騎士巡歴問題 {
                     for (Cell next : current.moves())
                         visit(i + 1, current, next);
                     current.unsetPrevious();
-                }
+                } else
+                    System.out.println(current + " already visited");
             }
 
             int run() {
@@ -106,12 +109,36 @@ class Test騎士巡歴問題 {
 
         }.run();
     }
-    
+
     static void print(Cell[][] board) {
         for (Cell[] row : board)
             System.out.println(Arrays.toString(
                 Arrays.stream(row).mapToInt(cell -> cell.order).toArray()));
         throw new RuntimeException();
+    }
+
+    @Test
+    void testBoard() {
+        int height = 4, width = 4;
+        Cell[][] board = Cell.board(height, width);
+        int[][] moveSizes = Stream.of(board)
+            .map(row -> Stream.of(row)
+                .mapToInt(cell -> cell.moves.size()).toArray())
+            .toArray(int[][]::new);
+        assertArrayEquals(
+            new int[][] {
+                {2, 3, 3, 2},
+                {3, 4, 4, 3},
+                {3, 4, 4, 3},
+                {2, 3, 3, 2},
+            }, moveSizes);
+        // 双方向リンクであることの確認
+        for (int row = 0; row < height; ++row)
+            for (int col = 0; col < width; ++col) {
+                Cell me = board[row][col];
+                for (Cell adjacent : me.moves)
+                    assertTrue(adjacent.moves.contains(me));
+            }
     }
 
     @Test

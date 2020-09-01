7f04c022c0ccbc61b679d8bf24bc1f7c858a18a7
saka1029 <saka1029@gmail.com>
2020-09-01 22:43:12 +0900

    aa

--- src/test/java/test/puzzle/TestCons.java
+++ src/test/java/test/puzzle/TestCons.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -232,6 +234,38 @@ class TestCons {
     public void testStream() {
         CList<Integer> clist = CList.of(1, 2, 3, 4);
         assertEquals(10, clist.stream().mapToInt(Integer::intValue).sum());
+    }
+    
+    static <T> List<T> cat(List<T> a, List<T> b) {
+        List<T> result = new ArrayList<>(a);
+        result.addAll(b);
+        return result;
+    }
+    
+    static <T> List<T> cons(T t, List<T> b) {
+        List<T> result = new ArrayList<>();
+        result.add(t);
+        result.addAll(b);
+        return result;
+    }
+
+    static <T> void perm(List<T> a, List<T> b) {
+        if (a.isEmpty())
+            System.out.println(b);
+        else
+            for (int i = 0; i < a.size(); ++i)
+                perm(cat(a.subList(0, i), a.subList(i + 1, a.size())), cons(a.get(i), b));
+    }
+
+    @Test
+    public void testSublist() {
+        List<Integer> list = List.of(1, 2, 3);
+//        perm(list, Collections.emptyList());
+        int[] rev = IntStream.range(0, list.size())
+            .map(i -> list.get(list.size() - i - 1))
+            .toArray();
+        System.out.println(Arrays.toString(rev));
+
     }
 
 }

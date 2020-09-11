2540ddef2645ab042ff6c9813aa41cca7dc52a62
saka1029 <saka1029@gmail.com>
2020-09-12 07:52:22 +0900

    aa

--- src/main/java/puzzle/Iterables.java
+++ src/main/java/puzzle/Iterables.java
@@ -1,11 +1,16 @@
 package puzzle;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.TreeMap;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.function.Predicate;
@@ -17,32 +22,31 @@ public class Iterables {
 
         final Iterator<T> source;
         final Predicate<T> predicate;
+        boolean hasNext;
         T next = null;
 
         FilterIterator(Iterator<T> source, Predicate<T> predicate) {
             this.source = source;
             this.predicate = predicate;
-            advance();
+            hasNext = advance();
         }
 
-        void advance() {
-            while (source.hasNext()) {
-                next = source.next();
-                if (predicate.test(next))
-                    return;
-            }
-            next = null;
+        boolean advance() {
+            while (source.hasNext())
+                if (predicate.test(next = source.next()))
+                    return true;
+            return false;
         }
 
         @Override
         public boolean hasNext() {
-            return next != null;
+            return hasNext;
         }
 
         @Override
         public T next() {
             T result = next;
-            advance();
+            hasNext = advance();
             return result;
         }
     }
@@ -80,7 +84,7 @@ public class Iterables {
         }
 
         IntRangeIterator(int from, int to) {
-            this(from, to, 1);
+            this(from, to, from <= to ? 1 : -1);
         }
 
         @Override
@@ -90,7 +94,7 @@ public class Iterables {
 
         @Override
         public Integer next() {
-            if (next >= to)
+            if (!hasNext())
                 throw new NoSuchElementException();
             int result = next;
             next += step;
@@ -122,6 +126,152 @@ public class Iterables {
         return () -> new MapIterator<T, U>(source.iterator(), mapper);
     }
 
+    public static <T extends Comparable<T>> Iterable<T> sorted(
+        Iterable<T> source) {
+        List<T> result = list(source);
+        Collections.sort(result);
+        return result;
+    }
+
+    public static <T> Iterable<T> sorted(Iterable<T> source,
+        Comparator<T> comparator) {
+        List<T> result = list(source);
+        Collections.sort(result, comparator);
+        return result;
+    }
+
+    public static <T> Iterable<T> reverse(Iterable<T> source) {
+        List<T> result = list(source);
+        Collections.reverse(result);
+        return result;
+    }
+
+    public static <T> Iterable<T> skip(Iterable<T> source, int skip) {
+        Iterator<T> iterator = source.iterator();
+        for (int i = 0; i < skip && iterator.hasNext(); ++i)
+            iterator.next();
+        return () -> iterator;
+    }
+
+    public static <T> Iterable<T> limit(Iterable<T> source, int limit) {
+        return () -> new Iterator<T>() {
+
+            int i = 0;
+            final Iterator<T> iterator = source.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return iterator.hasNext() && i < limit;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext())
+                    throw new NoSuchElementException();
+                ++i;
+                return iterator.next();
+            }
+
+        };
+    }
+
+    /*
+     * terminal operations
+     */
+    public static <T> List<T> list(Iterable<T> source) {
+        List<T> result = new ArrayList<>();
+        for (T e : source)
+            result.add(e);
+        return result;
+    }
+
+    public static <T> T[] array(Iterable<T> source,
+        IntFunction<T[]> supplier) {
+        return list(source).toArray(supplier);
+    }
+
+    public static <T> int[] array(Iterable<Integer> source) {
+        // サイズがわからないので一度リストにしてから配列にする必要があります。
+        List<Integer> list = list(source);
+        int size = list.size();
+        int[] result = new int[size];
+        for (int i = 0; i < size; ++i)
+            result[i] = list.get(i);
+        return result;
+    }
+
+    static <T, K, V> Map<K, V> map(Iterable<T> source,
+        Supplier<Map<K, V>> supplier, Function<T, K> keyExtractor,
+        Function<T, V> valueExtractor) {
+        Map<K, V> result = supplier.get();
+        for (T t : source)
+            result.put(keyExtractor.apply(t), valueExtractor.apply(t));
+        return result;
+    }
+
+    public static <T, K, V> HashMap<K, V> hashMap(Iterable<T> source,
+        Function<T, K> keyExtractor, Function<T, V> valueExtractor) {
+        return (HashMap<K, V>) map(source, () -> new HashMap<>(), keyExtractor,
+            valueExtractor);
+    }
+
+    public static <T, K, V> LinkedHashMap<K, V> linkedHashMap(
+        Iterable<T> source,
+        Function<T, K> keyExtractor, Function<T, V> valueExtractor) {
+        return (LinkedHashMap<K, V>) map(source, () -> new LinkedHashMap<>(),
+            keyExtractor, valueExtractor);
+    }
+
+    public static <T, K extends Comparable<K>, V> TreeMap<K, V> treeMap(
+        Iterable<T> source,
+        Function<T, K> keyExtractor, Function<T, V> valueExtractor) {
+        return (TreeMap<K, V>) map(source, () -> new TreeMap<>(), keyExtractor,
+            valueExtractor);
+    }
+
+    // public static <K, V> Map<K, V> toMap(Iterable<Entry<K, V>> source,
+    // Supplier<Map<K, V>> supplier) {
+    // Map<K, V> result = supplier.get();
+    // for (Entry<K, V> e : source)
+    // result.put(e.getKey(), e.getValue());
+    // return result;
+    // }
+    //
+    // public static <K, V> Map<K, V> toMap(Iterable<Entry<K, V>> source) {
+    // return toMap(source, () -> new HashMap<>());
+    // }
+
+    public static <T> T reduce(Iterable<T> source, T start,
+        BinaryOperator<T> reducer) {
+        for (T e : source)
+            start = reducer.apply(start, e);
+        return start;
+    }
+
+    public static int count(Iterable<Integer> source) {
+        int result = 0;
+        for (int i : source)
+            ++result;
+        return result;
+    }
+
+    public static int sum(Iterable<Integer> source) {
+        int result = 0;
+        for (int i : source)
+            result += i;
+        return result;
+    }
+
+    public static String join(Iterable<Integer> source, String separator) {
+        String sep = "";
+        StringBuilder sb = new StringBuilder();
+        for (int e : source) {
+            sb.append(sep).append(e);
+            sep = separator;
+        }
+        return sb.toString();
+    }
+
     /*
      * apply
      */
@@ -149,57 +299,4 @@ public class Iterables {
         Function<D, E> f4, Function<E, F> f5) {
         return f5.apply(f4.apply(f3.apply(f2.apply(f1.apply(a)))));
     }
-
-    /*
-     * terminal operations
-     */
-    public static <T> List<T> toList(Iterable<T> source) {
-        List<T> result = new ArrayList<>();
-        for (T e : source)
-            result.add(e);
-        return result;
-    }
-
-    public static <T> T[] toArray(Iterable<T> source,
-        IntFunction<T[]> supplier) {
-        return toList(source).toArray(supplier);
-    }
-
-    public static <T> int[] toArray(Iterable<Integer> source) {
-        // サイズがわからないので一度リストにしてから配列にする必要があります。
-        List<Integer> list = toList(source);
-        int size = list.size();
-        int[] result = new int[size];
-        for (int i = 0; i < size; ++i)
-            result[i] = list.get(i);
-        return result;
-    }
-
-    public static <T, K, V> Map<K, V> toMap(Iterable<T> source,
-        Supplier<Map<K, V>> supplier, Function<T, K> keyExtractor,
-        Function<T, V> valueExtractor) {
-        Map<K, V> result = supplier.get();
-        for (T t : source)
-            result.put(keyExtractor.apply(t), valueExtractor.apply(t));
-        return result;
-    }
-
-    public static <T, K, V> Map<K, V> toMap(Iterable<T> source,
-        Function<T, K> keyExtractor, Function<T, V> valueExtractor) {
-        return toMap(source, () -> new HashMap<>(), keyExtractor,
-            valueExtractor);
-    }
-
-//    public static <K, V> Map<K, V> toMap(Iterable<Entry<K, V>> source,
-//        Supplier<Map<K, V>> supplier) {
-//        Map<K, V> result = supplier.get();
-//        for (Entry<K, V> e : source)
-//            result.put(e.getKey(), e.getValue());
-//        return result;
-//    }
-//
-//    public static <K, V> Map<K, V> toMap(Iterable<Entry<K, V>> source) {
-//        return toMap(source, () -> new HashMap<>());
-//    }
-
 }
--- src/test/java/test/puzzle/TestIterables.java
+++ src/test/java/test/puzzle/TestIterables.java
@@ -13,35 +13,44 @@ import org.junit.jupiter.api.Test;
 class TestIterables {
 
     @Test
-    void testRangeIterator() {
-        assertEquals(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
-            toList(range(0, 10)));
-        assertEquals(List.of(0, 2, 4, 6, 8),
-            toList(filter(range(0, 10), i -> i % 2 == 0)));
-        assertEquals(List.of(100, 120, 140),
-            toList(map(filter(List.of(10, 11, 12, 13, 14, 15), i -> i % 2 == 0),
-                i -> i * 10)));
+    void testRange() {
+        assertEquals(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), list(range(0, 10)));
+        assertEquals(List.of(0, 2, 4, 6, 8), list(filter(range(0, 10), i -> i % 2 == 0)));
+        assertEquals(List.of(100, 120, 140), list(map(filter(List.of(10, 11, 12, 13, 14, 15), i -> i % 2 == 0), i -> i * 10)));
+        assertEquals(List.of(10, 8, 6, 4, 2), list(range(10, 0, -2)));
     }
 
     @Test
-    void testMapIterator() {
+    void testMap() {
         assertEquals(List.of(0, 2, 4, 6, 8),
-            toList(map(range(0, 5), i -> i * 2)));
+            list(map(range(0, 5), i -> i * 2)));
     }
+
+    @Test
+    void testSkip() {
+        assertEquals(List.of(5, 6, 7, 8, 9), list(skip(range(0, 10), 5)));
+    }
+
+    @Test
+    void testLimit() {
+        assertEquals(List.of(0, 1, 2, 3, 4), list(limit(range(0, 10), 5)));
+        assertEquals(List.of(3, 4, 5), list(limit(skip(range(0, 10), 3), 3)));
+    }
+
 
     @Test
     void testToArray() {
         assertArrayEquals(new Integer[] {0, 1, 2, 3},
-            toArray(range(0, 4), Integer[]::new));
-        assertArrayEquals(new int[] {0, 1, 2, 3}, toArray(range(0, 4)));
+            array(range(0, 4), Integer[]::new));
+        assertArrayEquals(new int[] {0, 1, 2, 3}, array(range(0, 4)));
     }
 
     @Test
     void testNoApply() {
         assertEquals(List.of(0, 2, 4, 6, 8),
-            toList(filter(range(0, 10), i -> i % 2 == 0)));
+            list(filter(range(0, 10), i -> i % 2 == 0)));
         assertEquals(List.of(100, 120, 140),
-            toList(map(filter(List.of(10, 11, 12, 13, 14, 15),
+            list(map(filter(List.of(10, 11, 12, 13, 14, 15),
                 i -> i % 2 == 0),
                 i -> i * 10)));
     }
@@ -51,12 +60,12 @@ class TestIterables {
         assertEquals(List.of(0, 2, 4, 6, 8),
             apply(range(0, 10),
                 r -> filter(r, i -> i % 2 == 0),
-                r -> toList(r)));
+                r -> list(r)));
         assertEquals(List.of(100, 120, 140),
             apply(List.of(10, 11, 12, 13, 14, 15),
                 r -> filter(r, i -> i % 2 == 0),
                 r -> map(r, i -> i * 10),
-                r -> toList(r)));
+                r -> list(r)));
     }
 
     static Iterable<Integer> primes(int max) {
@@ -72,21 +81,52 @@ class TestIterables {
     @Test
     void testPrimes() {
         assertEquals(List.of(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
-            47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97), toList(primes(100)));
+            47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97), list(primes(100)));
     }
 
     @Test
-    void testMap() {
+    void testHashMap() {
         List<String> list = List.of("zero", "one", "two");
         assertEquals(Map.of(0, "zero", 1, "one", 2, "two"),
             apply(range(0, list.size()),
-                s -> toMap(s, Function.identity(), list::get)));
+                s -> hashMap(s, Function.identity(), list::get)));
         // assertEquals(Map.of(0, "zero", 1, "one", 2, "two"),
-        // TODO: 無名のObjectは使えない。
+        // TODO: 無名のObjectはapplyの中では使えない。
         // apply(range(0, list.size()),
         // s -> map(s, i -> new Object() { int k = i; String v = list.get(i);
         // }),
         // s -> toMap(s, obj -> obj.k, obj -> obj.v)));
+        assertEquals(Map.of(0, "zero", 1, "one", 2, "two"),
+            hashMap(map(range(0, list.size()), i -> new Object() {
+                final int k = i;
+                final String v = list.get(i);
+            }), obj -> obj.k, obj -> obj.v));
+        var s = map(range(0, list.size()), i -> new Object() {
+                int k = i;
+                String v = list.get(i);
+            });
+        assertEquals(Map.of(0, "zero", 1, "one", 2, "two"),
+            hashMap(s, obj -> obj.k, obj -> obj.v));
+    }
+
+    @Test
+    void testReduce() {
+        assertEquals(10, reduce(range(0, 5), 0, (a, b) -> a + b));
+        assertEquals(10, reduce(range(0, 5), 0, Integer::sum));
+        assertEquals(120, reduce(range(1, 6), 1, (a, b) -> a * b));
+        assertEquals(0, reduce(List.of(), 0, (a, b) -> a + b));
+    }
+
+    @Test
+    void testSum() {
+        assertEquals(10, sum(range(0, 5)));
+        assertEquals(0, sum(List.of()));
+    }
+
+    @Test
+    void testJoin() {
+        assertEquals("0, 1, 2, 3, 4", join(range(0, 5), ", "));
+        assertEquals("", join(List.of(), ", "));
     }
 
 }

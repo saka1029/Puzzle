bd2ebf97e191d0600f022b5fcb937db2dcc51c9c
saka1029 <saka1029@gmail.com>
2021-09-25 18:29:32 +0900

    Create TestVariableCalculator.java

---
+++ src/test/java/test/puzzle/parsers/TestVariableCalculator.java
@@ -0,0 +1,14 @@
+package test.puzzle.parsers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestVariableCalculator {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}

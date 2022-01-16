2f4d3aaae4f07898d2dd5c7c199a6f73b298feeb
saka1029 <saka1029@gmail.com>
2022-01-16 14:00:11 +0900

    Update Test施設基準parser.java

--- src/test/java/test/puzzle/parsers/Test施設基準parser.java
+++ src/test/java/test/puzzle/parsers/Test施設基準parser.java
@@ -2,6 +2,7 @@ package test.puzzle.parsers;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.text.Normalizer;
 import java.util.regex.Matcher;
@@ -137,6 +138,45 @@ class Test施設基準parser {
             return isMatch(a) && isMatch(b);
         }
     }
+    
+    static class 第漢数字 extends 漢数字 {
+        第漢数字(String s) {
+            super(s);
+        }
+
+        第漢数字() {
+            this("第" + 漢数字列の);
+        }
+        
+        @Override
+        public boolean isFirst(String 項番) {
+            return id(項番).equals("1");
+        }
+        
+        static final Pattern 最後の数字 = Pattern.compile("\\d+$");
+
+        String nextId(String id) {
+            return 最後の数字.matcher(id).replaceAll(m -> "" + (Integer.parseInt(m.group()) + 1));
+        }
+        
+        String nextId2(String id) {
+            return id + "-2";
+        }
+        
+        @Override
+        public boolean isNext(String 項番1, String 項番2) {
+            if (!isMatch(項番1) || !isMatch(項番2))
+                return false;
+            String id1 = id(項番1), id2 = id(項番2);
+            return id2.equals(nextId(id1)) || id2.equals(nextId2(id1));
+        }
+    }
+    
+    static class 漢数字章 extends 第漢数字 {
+        漢数字章() {
+            super(漢数字列の + "章");
+        }
+    }
 
     static class 単一文字項番 implements 項番 {
         final String all;
@@ -184,40 +224,86 @@ class Test施設基準parser {
 
     @Test
     void test数字() {
-        assertEquals(12, new 数字().number("12"));
-        assertEquals(12, new 数字().number("１２"));
-        assertTrue(new 数字().isNext("3", "4"));
+        数字 x = new 数字();
+        assertEquals(12, x.number("12"));
+        assertEquals(12, x.number("１２"));
+        assertTrue(x.isNext("3", "4"));
     }
 
     @Test
     void test括弧数字() {
-        assertEquals(12, new 括弧数字().number("(12)"));
-        assertEquals(12, new 括弧数字().number("（12）"));
-        assertEquals(12, new 括弧数字().number("（１２）"));
-        assertTrue(new 括弧数字().isNext("（１２）", "(13)"));
+        括弧数字 x = new 括弧数字();
+        assertEquals(12, x.number("(12)"));
+        assertEquals(12, x.number("（12）"));
+        assertEquals(12, x.number("（１２）"));
+        assertTrue(x.isFirst("（１）"));
+        assertFalse(x.isFirst("（１2）"));
+        assertTrue(x.isNext("（１２）", "(13)"));
+        assertEquals("(12)", x.id("（１2）"));
     }
 
     @Test
     void test丸数字() {
-        assertEquals(12, new 丸数字().number("⑫"));
-        assertTrue(new 丸数字().isNext("⑫", "⑬"));
+        丸数字 x = new 丸数字();
+        assertEquals(12, x.number("⑫"));
+        assertTrue(x.isFirst("①"));
+        assertFalse(x.isFirst("②"));
+        assertTrue(x.isNext("⑫", "⑬"));
     }
 
     @Test
     void testイロハ() {
-        assertEquals(12, new イロハ().number("ヲ"));
-        assertEquals("12", new イロハ().id("ヲ"));
-        assertTrue(new イロハ().isNext("ヲ", "ワ"));
+        イロハ x = new イロハ();
+        assertEquals(12, x.number("ヲ"));
+        assertEquals("12", x.id("ヲ"));
+        assertTrue(x.isFirst("イ"));
+        assertFalse(x.isFirst("ワ"));
+        assertTrue(x.isNext("ヲ", "ワ"));
     }
 
     @Test
     void test漢数字() {
-        assertTrue(new 漢数字().pattern().matcher("三").matches());
-        assertTrue(new 漢数字().pattern().matcher("十二").matches());
-        assertTrue(new 漢数字().pattern().matcher("三十三から三十三の五まで").matches());
-        assertTrue(new 漢数字().pattern().matcher("三十四及び三十五").matches());
-        assertEquals("35-5-2", new 漢数字().id("三十五の五の二"));
-        assertEquals("34:35-5-2", new 漢数字().id("三十四及び三十五の五の二まで"));
+        漢数字 x = new 漢数字();
+        assertTrue(x.isMatch("三"));
+        assertTrue(x.isMatch("十二"));
+        assertTrue(x.isMatch("三十三から三十三の五まで"));
+        assertTrue(x.isMatch("三十四及び三十五"));
+        assertTrue(x.isFirst("一"));
+        assertTrue(x.isFirst("三"));
+        assertEquals("35-5-2", x.id("三十五の五の二"));
+        assertEquals("34:35-5-2", x.id("三十四及び三十五の五の二まで"));
+    }
+
+    @Test
+    void test第漢数字() {
+        第漢数字 x = new 第漢数字();
+        assertTrue(x.isMatch("第三"));
+        assertTrue(x.isMatch("第十二"));
+        assertTrue(x.isMatch("第三十三の五"));
+        assertTrue(x.isMatch("第三十四の二の二"));
+        assertTrue(x.isFirst("第一"));
+        assertFalse(x.isFirst("第三"));
+        assertEquals("4", x.nextId(x.id("第三")));
+        assertEquals("3-2", x.nextId2(x.id("第三")));
+        assertTrue(x.isNext("第三", "第四"));
+        assertTrue(x.isNext("第三", "第三の二"));
+        assertEquals("35-5-2", x.id("第三十五の五の二"));
+    }
+
+    @Test
+    void test漢数字章() {
+        漢数字章 x = new 漢数字章();
+        assertTrue(x.isMatch("三章"));
+        assertTrue(x.isMatch("十二章"));
+        assertTrue(x.isMatch("三十三の五章"));
+        assertTrue(x.isMatch("三十四の二の二章"));
+        assertTrue(x.isFirst("一章"));
+        assertFalse(x.isFirst("三章"));
+        assertEquals("4", x.nextId(x.id("三章")));
+        assertEquals("3-2", x.nextId2(x.id("三章")));
+        assertTrue(x.isNext("三章", "四章"));
+        assertTrue(x.isNext("三章", "三の二章"));
+        assertEquals("35-5-2", x.id("三十五の五の二章"));
     }
 
 }

fb9e695d9411481189ad6f45a7d2a5baa0b0599c
saka1029 <saka1029@gmail.com>
2022-01-19 16:22:42 +0900

    Update DoubleFormula.java

--- src/main/java/puzzle/parsers/DoubleFormula.java
+++ src/main/java/puzzle/parsers/DoubleFormula.java
@@ -1,34 +1,47 @@
 package puzzle.parsers;
 
-import java.util.function.DoubleUnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DoubleFormula {
+    interface DoubleF {
+        double apply(double x);
+        default DoubleF add(DoubleF f) { return x -> apply(x) + f.apply(x); }
+    }
+
     static Pattern TERM = Pattern.compile(
         "([+-])?(?:(\\d+)?x(?:\\^(\\d+))?|(\\d+))");
 
-    static DoubleUnaryOperator parse(String formula) {
-        DoubleUnaryOperator y = x -> 0;
+    /**
+     * <pre>
+     * SYNTAX:
+     *   formula  = term { term }
+     *   term     = [ "+" | "-" ] ( variable | constant )
+     *   variable = [ DIGITS ] "x" [ "^" DIGITS ]
+     *   constant = DIGITS
+     * </pre>
+     * SYNTAX:
+     */
+    static DoubleF parse(String formula) {
+        DoubleF f = x -> 0;
         Matcher m = TERM.matcher(formula.toLowerCase());
         while (m.find()) {
-            DoubleUnaryOperator f = y;
             double sign = "-".equals(m.group(1)) ? -1 : 1;
             if (m.group(4) != null) {
-                 double v = sign * Double.parseDouble(m.group(4));
-                 y = x -> f.applyAsDouble(x) + v;
+                 double c = sign * Double.parseDouble(m.group(4));
+                 f = f.add(x -> c);
             } else {
-                double coef = sign * (m.group(2) == null ? 1 : Double.parseDouble(m.group(2)));
-                double exp = m.group(3) == null ? 1 : Double.parseDouble(m.group(3));
-                y = x -> f.applyAsDouble(x) + coef * (float)Math.pow(x, exp);
+                double c = sign * (m.group(2) == null ? 1 : Double.parseDouble(m.group(2)));
+                double e = m.group(3) == null ? 1 : Double.parseDouble(m.group(3));
+                f = f.add(x -> c * (float)Math.pow(x, e));
             }
         }
-        return y;
+        return f;
     }
 
     public static void main(String[] args) {
-        DoubleUnaryOperator f = parse("+5x^2+x+2");
+        DoubleF f = parse("+5x^2+x+2");
         for (double i = 0; i < 10; i++)
-            System.out.println("f(" + i + ") = " + f.applyAsDouble(i));
+            System.out.println("f(" + i + ") = " + f.apply(i));
     }
 }

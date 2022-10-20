44c2969d76b30ac2c8501b7aae905ea0790fd128
saka1029 <saka1029@gmail.com>
2022-10-20 17:07:07 +0900

    Create Expression.java

---
+++ src/main/java/puzzle/core/Expression.java
@@ -0,0 +1,19 @@
+package puzzle.core;
+
+import java.util.Map;
+
+/**
+ * 1st version
+ * expression = [ "+" | "-" ] term { ("+" | "-") term }
+ * term       = element { ( "*" | "/" ) element }
+ * element    = "(" expression ")" | variable | number
+ * 
+ * expression = [ "+" | "-" ] term { ("+" | "-") term }
+ * term       = factor { ("*" | "/") factor }
+ * factor     = primary [ "^" factor ]
+ * primary    = primary | element
+ * element    = "(" expression ")" | variable | number
+ */
+public interface Expression {
+    double eval(Map<String, Double> variables);
+}

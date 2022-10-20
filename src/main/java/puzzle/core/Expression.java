b1cdc7551b02c9bf97876b3972325d0c7ca0dc5e
saka1029 <saka1029@gmail.com>
2022-10-20 22:45:41 +0900

    TestExpression追加。

--- src/main/java/puzzle/core/Expression.java
+++ src/main/java/puzzle/core/Expression.java
@@ -1,30 +1,50 @@
 package puzzle.core;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
+ * 数式をコンパイルします。
+ * 
  * <pre>
  * *** 1st version ***
  * expression = term { ("+" | "-" ) term }
  * term       = factor { ( "*" | "/" ) factor }
  * factor    = [ "-" ] ( "(" expression ")" | variable | number )
  * </pre>
- * 
- * <pre>
- * expression = term { ("+" | "-") term }
- * term       = factor { ("*" | "/" ) factor }
- * factor     = primary [ "^" factor ]
- * primary    = primary | element
- * element    = [ "-" ] ( "(" expression ")" | variable | number )
- * </pre>
  */
 public interface Expression {
 
     double eval(Map<String, Double> variables);
 
+    public class ParseException extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+        ParseException(String format, Object... args) {
+            super(format.formatted(args));
+        }
+    }
+
     public static Expression of(String s) {
         return new Object() {
             int length = s.length(), index = 0, ch = get();
+            StringBuilder sb = new StringBuilder();
+            
+            void bufferClear() {
+                sb.setLength(0);
+            }
+            
+            void bufferAppend(int ch) {
+                sb.append((char)ch);
+            }
+            
+            void bufferAppendGet(int ch) {
+                bufferAppend(ch);
+                get();
+            }
+            
+            String bufferString() {
+                return sb.toString();
+            }
             
             int get() {
                 return ch = index < length ? s.charAt(index++) : -1;
@@ -44,8 +64,65 @@ public interface Expression {
                 return false;
             }
             
+            Expression paren() {
+                Expression e = expression();
+                if (!eat(')'))
+                    throw new ParseException("')' expected");
+                return e;
+            }
+            
+            Expression number() {
+                bufferClear();
+                while (Character.isDigit(ch))
+                    bufferAppendGet(ch);
+                if (ch == '.') {
+                    bufferAppendGet(ch);
+                    while (Character.isDigit(ch))
+                        bufferAppendGet(ch);
+                }
+                if (ch == 'e' || ch == 'E') {
+                    bufferAppendGet(ch);
+                    if (ch == '-')
+                        bufferAppendGet(ch);
+                    while (Character.isDigit(ch))
+                        bufferAppendGet(ch);
+                }
+                double value = Double.parseDouble(bufferString());
+                return v -> value;
+            }
+            
+            Expression variable() {
+                bufferClear();
+                bufferAppendGet(ch);
+                while (Character.isAlphabetic(ch) || Character.isDigit(ch) || ch == '_')
+                    bufferAppendGet(ch);
+                String name = bufferString();
+                return v -> {
+                    Double value = v.get(name);
+                    if (value == null)
+                        throw new RuntimeException("variable `%s` not defined".formatted(name));
+                    return value;
+                };
+            }
+
             Expression factor() {
-                
+                Expression e;
+                boolean minus = false;
+                if (eat('-'))
+                    minus = true;
+                if (eat('('))
+                    e = paren();
+                else if (Character.isDigit(ch))
+                    e = number();
+                else if (Character.isAlphabetic(ch))
+                    e = variable();
+                else
+                    throw new ParseException("unknown char '%c'", (char)ch);
+                if (minus) {
+                    Expression org = e;
+                    e = v -> -org.eval(v);
+                }
+                return e;
             }
 
             Expression term() {
@@ -61,7 +138,6 @@ public interface Expression {
                         break;
                 }
                 return e;
-                
             }
 
             Expression expression() {
@@ -81,7 +157,18 @@ public interface Expression {
 
             Expression parse() {
                 Expression e = expression();
-                return e;
+                if (index < length)
+                    throw new ParseException("extra string '%s'", s.substring(index - 1));
+                return new Expression() {
+                    @Override
+                    public double eval(Map<String, Double> variables) {
+                        return e.eval(variables);
+                    }
+                    @Override
+                    public String toString() {
+                        return s;
+                    }
+                };
             }
             
         }.parse();
---
+++ src/test/java/test/puzzle/core/TestExpression.java
@@ -0,0 +1,70 @@
+package test.puzzle.core;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import puzzle.core.Expression;
+
+public class TestExpression {
+
+    static final double E = 0.00005;
+
+    @Test
+    public void testNumber() {
+        assertEquals(12.3, Expression.of("12.3").eval(Map.of()), E);
+        assertEquals(-12.3, Expression.of("-12.3").eval(Map.of()), E);
+    }
+
+    @Test
+    public void testVariable() {
+        assertEquals(12.3, Expression.of("xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(-12.3, Expression.of("-xyz").eval(Map.of("xyz", 12.3)), E);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("12.3", Expression.of("12.3").toString());
+        assertEquals("-xyz", Expression.of("-xyz").toString());
+        assertEquals("12.3 - xyz", Expression.of("12.3 - xyz").toString());
+    }
+    
+    @Test
+    public void testPlus() {
+        assertEquals(15.75, Expression.of("3.45 + xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(15.75, Expression.of("xyz + 3.45").eval(Map.of("xyz", 12.3)), E);
+    }
+    
+    @Test
+    public void testMinus() {
+        assertEquals(-8.85, Expression.of("3.45 - xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(8.85, Expression.of("xyz - 3.45").eval(Map.of("xyz", 12.3)), E);
+    }
+    
+    @Test
+    public void testMultiply() {
+        assertEquals(42.435, Expression.of("3.45 * xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(42.435, Expression.of("xyz * 3.45").eval(Map.of("xyz", 12.3)), E);
+    }
+    
+    @Test
+    public void testDivide() {
+        assertEquals(0.280487804878049, Expression.of("3.45 / xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(3.56521739130435, Expression.of("xyz / 3.45").eval(Map.of("xyz", 12.3)), E);
+    }
+    
+    @Test
+    public void testPlusMult() {
+        assertEquals(38.9, Expression.of("2 + 3 * xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(38.9, Expression.of("3 * xyz + 2").eval(Map.of("xyz", 12.3)), E);
+    }
+    
+    @Test
+    public void testParen() {
+        assertEquals(61.5, Expression.of("(2 + 3) * xyz").eval(Map.of("xyz", 12.3)), E);
+        assertEquals(42.9, Expression.of("3 * (xyz + 2)").eval(Map.of("xyz", 12.3)), E);
+    }
+
+}

d3db7415f2caffde4898d7b00f5dabc95ff24572
saka1029 <saka1029@gmail.com>
2022-10-20 19:48:57 +0900

    Update Expression.java

--- src/main/java/puzzle/core/Expression.java
+++ src/main/java/puzzle/core/Expression.java
@@ -3,17 +3,45 @@ package puzzle.core;
 import java.util.Map;
 
 /**
- * 1st version
- * expression = [ "-" ] term { ("+" | "-") term }
- * term       = element { ( "*" | "/" ) element }
- * element    = "(" expression ")" | variable | number
+ * <pre>
+ * *** 1st version ***
+ * expression = term { ("+" | "-" ) term }
+ * term       = factor { ( "*" | "/" ) factor }
+ * factor    = [ "-" ] ( "(" expression ")" | variable | number )
+ * </pre>
  * 
- * expression = [ "-" ] term { ("+" | "-") term }
- * term       = factor { ("*" | "/") factor }
+ * <pre>
+ * expression = term { ("+" | "-") term }
+ * term       = factor { ("*" | "/" ) factor }
  * factor     = primary [ "^" factor ]
  * primary    = primary | element
- * element    = "(" expression ")" | variable | number
+ * element    = [ "-" ] ( "(" expression ")" | variable | number )
+ * </pre>
  */
 public interface Expression {
+
     double eval(Map<String, Double> variables);
+
+    public static Expression of(String s) {
+        return new Object() {
+            int length = s.length(), index = 0, ch = get();
+            
+            int get() {
+                return ch = index < length ? s.charAt(index++) : -1;
+            }
+            
+            boolean eat(int expect) {
+                if (ch == expect) {
+                    get();
+                    return true;
+                }
+                return false;
+            }
+            
+            Expression parse() {
+                return v -> 1.23;
+            }
+            
+        }.parse();
+    }
 }

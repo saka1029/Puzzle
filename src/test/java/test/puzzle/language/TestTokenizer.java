5cd3369b1963b4d649c06fd750087d75c922470a
saka1029 <saka1029@gmail.com>
2022-10-31 19:36:10 +0900

    Update TestTokenizer.java

--- src/test/java/test/puzzle/language/TestTokenizer.java
+++ src/test/java/test/puzzle/language/TestTokenizer.java
@@ -1,5 +1,43 @@
 package test.puzzle.language;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Objects;
+
+import org.junit.Test;
+
 public class TestTokenizer {
+    
+    public static class Tokenizer {
+
+        public final String source;
+
+        int index;
+        public int ch;
+        
+        public Tokenizer(String source) {
+            this.source = source;
+            getCh();  // 1文字先読み
+        }
+        
+        public int getCh() {
+            if (index < source.length()) {
+                ch = source.codePointAt(index);
+                index += Character.isSupplementaryCodePoint(ch) ? 2 : 1;
+            } else
+                ch = -1;
+            return ch;
+        }
+    }
+    
+    @Test
+    public void testGet() {
+        Tokenizer t = new Tokenizer("a𩸽b");
+        assertEquals('a', t.ch); t.getCh();
+        assertEquals("𩸽".codePointAt(0), t.ch); t.getCh();
+        assertEquals('b', t.ch); t.getCh();
+        assertEquals(-1, t.ch); t.getCh();
+    }
+    
 
 }

83c37bdbcf919b9b5e929a4fd75c7eee52d964e1
saka1029 <saka1029@gmail.com>
2022-11-01 18:53:21 +0900

    Update TestTokenizer.java

--- src/test/java/test/puzzle/language/TestTokenizer.java
+++ src/test/java/test/puzzle/language/TestTokenizer.java
@@ -3,16 +3,28 @@ package test.puzzle.language;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Objects;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class TestTokenizer {
     
+    public static class TokenizerException extends Exception {
+        private static final long serialVersionUID = 1L;
+        public TokenizerException(String format, Object... args) {
+            super(format.formatted(args));
+        }
+    }
+    
     public static class Tokenizer {
+
+        public static final int TOKEN_BASE = 1 << 21;
+        public static final int TOKEN_NUMBER = TOKEN_BASE + 1;
+        public static final int TOKEN_ID = TOKEN_BASE + 2;
 
         public final String source;
 
-        int index;
+        int nextIndex = 0, index = 0;
         public int ch;
         
         public Tokenizer(String source) {
@@ -21,12 +33,46 @@ public class TestTokenizer {
         }
         
         public int getCh() {
-            if (index < source.length()) {
-                ch = source.codePointAt(index);
-                index += Character.isSupplementaryCodePoint(ch) ? 2 : 1;
+            if (nextIndex < source.length()) {
+                ch = source.codePointAt(nextIndex);
+                index = nextIndex;
+                nextIndex += Character.isSupplementaryCodePoint(ch) ? 2 : 1;
             } else
                 ch = -1;
             return ch;
+        }
+        
+        public void skipSpaces() {
+            while (Character.isWhitespace(ch))
+                getCh();
+        }
+        
+        public boolean isOneCharToken(int ch) {
+            switch (ch) {
+            case '(': case ')':
+            case '+': case '-':
+            case '*': case '/':
+            case '=':
+                return true;
+            default:
+                return false;
+            }
+        }
+        
+        public boolean isDigit(int ch) {
+            return ch >= '0' && ch <= '9';
+        }
+        
+        public int get() throws TokenizerException {
+            skipSpaces();
+            if (isOneCharToken(ch)) {
+                int type = ch;
+                getCh();
+                return type;
+            } else if (isDigit(ch)) {
+                
+            }
+            throw new TokenizerException("unknown char '%c'", ch);
         }
     }
     
@@ -37,6 +83,11 @@ public class TestTokenizer {
         assertEquals("𩸽".codePointAt(0), t.ch); t.getCh();
         assertEquals('b', t.ch); t.getCh();
         assertEquals(-1, t.ch); t.getCh();
+    }
+    
+    @Test
+    public void testToken() {
+        System.out.println(Tokenizer.TOKEN_BASE);
     }
     
 

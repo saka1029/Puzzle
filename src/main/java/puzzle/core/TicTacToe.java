9080641a5dabae85199d06968de4d68941f79947
saka1029 <saka1029@gmail.com>
2022-12-03 18:04:53 +0900

    ALL追加。

--- src/main/java/puzzle/core/TicTacToe.java
+++ src/main/java/puzzle/core/TicTacToe.java
@@ -1,9 +1,13 @@
 package puzzle.core;
 
+import java.util.Arrays;
+
 /**
- * 手番は1,2で表現する。 局面(board)は整数で表現する。 局面は空き(0),手番1(1)、手番2(2)の9個の組み合わせで表現する。
- * 局面の種類は3^9 = 19683通りある。 short[3^9][2][9] ALLはすべての局面とその可能な次の手を持つ。
- * ALL[n]は局面nにおける次の手を表す。
+ * 手番は1,2で表現する。 局面(board)は整数で表現する。
+ * 局面は空き(0),手番1(1)、手番2(2)の9個の組み合わせで表現します。
+ * 局面の種類は3^9 = 19683通りあります。
+ * short[3^9][2][9] ALLはすべての局面とその可能な次の手を持ちます。
+ * ALL[n]は局面nにおける次の手を表します。
  * 
  */
 public class TicTacToe {
@@ -15,11 +19,6 @@ public class TicTacToe {
     public static final int SIZE = 9;
     public static final int RADIX = 3, ROWS = 3, COLS = 3;
     public static final int[] POW3 = {1, 3, 9, 27, 81, 243, 729, 2187, 6561};
-    public static final int[][] PATHS = {
-        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal
-        {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical
-        {0, 4, 8}, {2, 4, 6} // diagonal
-    };
 
     /**
      * 9個の要素の並びから局面を求めます。
@@ -45,6 +44,23 @@ public class TicTacToe {
         for (int i = SIZE - 1; i >= 0 && board > 0; --i, board /= RADIX)
             result[i] = board % RADIX;
         return result;
+    }
+    
+    public static final String[] CHARS = {" ", "x", "o"};
+    /**
+     * 局面の文字列表現を返します。
+     * 3進数の最下位の桁が左上隅に配置されます。
+     * @param board 局面
+     * @return
+     */
+    public static String string(int board) {
+        StringBuilder sb = new StringBuilder();
+        for (int r = 0; r < ROWS; ++r) {
+            for (int c = 0; c < COLS; ++c, board /= RADIX)
+                sb.append(CHARS[board % RADIX]);
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
     }
 
     /**
@@ -83,16 +99,21 @@ public class TicTacToe {
      * - n = 0の時0を返します。 上記以外の場合は-1を返します。
      * 
      * @param board
-     * @return 手番1の場合1、手番2の場合2、どちらの手番もありうる場合0、 次の手番があり得ない場合-1を返します。
-     *         手番1と2の着手数の差の絶対値が1より大きい場合は次の手番はあり得ません。
+     * @return 手番1の場合は1、手番2の場合は2、どちらの手番もありうる場合は0、
+     *         次の手番があり得ない場合は-1を返します。
+     *         (1) 空いているマスがない場合は次の手番はあり得ません。
+     *         (2) 手番1と2の着手数の差の絶対値が1より大きい場合は次の手番はあり得ません。
      */
     public static int next(int board) {
-        int one = 0, two = 0;
-        for (int i = 0; i < SIZE && board > 0; ++i, board /= RADIX)
+        int zero = 0, one = 0, two = 0;
+        for (int i = 0; i < SIZE; ++i, board /= RADIX)
             switch (board % RADIX) {
+                case 0: ++zero; break;
                 case 1: ++one; break;
                 case 2: ++two; break;
             }
+        if (zero == 0)
+            return -1;
         return switch (one - two) {
             case 1 -> 2;
             case -1 -> 1;
@@ -101,20 +122,67 @@ public class TicTacToe {
         };
     }
 
+    public static final int[][] PATHS = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical
+        {0, 4, 8}, {2, 4, 6} // diagonal
+    };
+
     /**
-     * 
+     * 勝者を返します。
      * @param board
      * @return 手番1が勝っている場合は1、
-     *         手番2が勝っている場合は2、
-     *         未決着の場合は0
-     *         ありえない局面の場合は-1を返します。
+     * 手番2が勝っている場合は2、
+     * 未決着の場合は0、
+     * ありえない局面の場合は-1を返します。
+     * (両者に並びがある場合)
      */
     public static int winner(int board) {
         int[] array = array(board);
+        int one = 0, two = 0;
         for (int[] path : PATHS) {
-            
+            int p0 = array[path[0]], p1 = array[path[1]], p2 = array[path[2]];
+            if (p0 == p1 && p0 == p2) {
+                if (p0 == 1)
+                    ++one;
+                else if (p0 == 2)
+                    ++two;
+            }
         }
+        if (one > 0 && two == 0)
+            return 1;
+        else if (one == 0 && two > 0)
+            return 2;
+        else if (one == 0 && two == 0)
+            return 0;
+        else
+            return -1;
+    }
+    
+    public static final short[][][] ALL = all();
 
+    static short[] next(int board, int next) {
+        short[] r = new short[SIZE];
+        for (int i = 0; i < SIZE; ++i)
+            r[i] = get(board, i) == 0 ? (short)set(board, i, next) : -1;
+        return r;
+    }
+
+    public static short[][][] all() {
+        short[][][] all = new short[MAX_BOARD][][];
+        for (int i = 0; i < MAX_BOARD; ++i) {
+            int next = next(i);
+            if (next == -1)
+                continue;
+            if (winner(i) != 0)
+                continue;
+            short[][] e = all[i] = new short[2][];
+            if (next == 0 || next == 1)
+                e[0] = next(i, 1);
+            if (next == 0 || next == 2)
+                e[1] = next(i, 2);
+        }
+        return all;
     }
 
 }
--- src/test/java/test/puzzle/core/TestTicTacToe.java
+++ src/test/java/test/puzzle/core/TestTicTacToe.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static puzzle.core.TicTacToe.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class TestTicTacToe {
@@ -23,6 +25,11 @@ public class TestTicTacToe {
     }
     
     @Test
+    public void testString() {
+        assertEquals(" xo%n xo%n xo%n".formatted(), string(board(2, 1, 0, 2, 1, 0, 2, 1, 0)));
+    }
+    
+    @Test
     public void testGet() {
         assertEquals(2, get(MAX_BOARD - 1, 0));
         assertEquals(2, get(MAX_BOARD - 1, 8));
@@ -31,5 +38,64 @@ public class TestTicTacToe {
         assertEquals(1, get(board(2, 1, 0, 2, 1, 0, 2, 1, 0), 4));
         assertEquals(2, get(board(2, 1, 0, 2, 1, 0, 2, 1, 0), 5));
     }
-
+    
+    @Test
+    public void testSet() {
+        assertEquals(board(2, 1, 0, 2, 1, 0, 2, 1, 2), set(board(2, 1, 0, 2, 1, 0, 2, 1, 0), 0, 2));
+        assertEquals(board(0, 1, 0, 2, 1, 0, 2, 1, 0), set(board(2, 1, 0, 2, 1, 0, 2, 1, 0), 8, 0));
+        assertEquals(board(2, 1, 0, 2, 2, 0, 2, 1, 0), set(board(2, 1, 0, 2, 1, 0, 2, 1, 0), 4, 2));
+    }
+    
+    @Test
+    public void testNext() {
+        assertEquals(0, next(board(0, 0, 0, 0, 0, 0, 0, 0, 0)));
+        assertEquals(1, next(board(0, 0, 0, 0, 0, 0, 0, 0, 2)));
+        assertEquals(2, next(board(1, 0, 0, 0, 0, 0, 0, 0, 0)));
+        assertEquals(0, next(board(1, 0, 0, 0, 0, 0, 0, 0, 2)));
+        assertEquals(-1, next(board(1, 0, 0, 1, 1, 0, 0, 0, 2)));
+        assertEquals(-1, next(board(1, 2, 0, 2, 0, 0, 0, 0, 2)));
+    }
+    
+    @Test
+    public void testWinner() {
+        assertEquals(1, winner(board(1, 1, 1, 1, 1, 1, 1, 1, 1)));
+        assertEquals(0, winner(board(0, 0, 0, 0, 0, 0, 0, 0, 0)));
+        assertEquals(0, winner(board(0, 0, 0, 0, 0, 0, 0, 0, 2)));
+        assertEquals(0, winner(board(1, 0, 0, 0, 0, 0, 0, 0, 0)));
+        assertEquals(0, winner(board(1, 0, 0, 0, 0, 0, 0, 0, 2)));
+        assertEquals(0, winner(board(1, 0, 0, 1, 1, 0, 0, 0, 2)));
+        assertEquals(0, winner(board(1, 2, 0, 2, 0, 0, 0, 0, 2)));
+        assertEquals(1, winner(board(0, 0, 0, 0, 0, 0, 1, 1, 1)));
+        assertEquals(1, winner(board(0, 0, 0, 1, 1, 1, 0, 0, 0)));
+        assertEquals(1, winner(board(1, 1, 1, 0, 0, 0, 0, 0, 0)));
+        assertEquals(1, winner(board(0, 0, 1, 0, 0, 1, 0, 0, 1)));
+        assertEquals(1, winner(board(0, 1, 0, 0, 1, 0, 0, 1, 0)));
+        assertEquals(1, winner(board(1, 0, 0, 1, 0, 0, 1, 0, 0)));
+        assertEquals(1, winner(board(1, 0, 0, 0, 1, 0, 0, 0, 1)));
+        assertEquals(1, winner(board(0, 0, 1, 0, 1, 0, 1, 0, 0)));
+        assertEquals(2, winner(board(0, 0, 0, 0, 0, 0, 2, 2, 2)));
+        assertEquals(2, winner(board(0, 0, 0, 2, 2, 2, 0, 0, 0)));
+        assertEquals(2, winner(board(2, 2, 2, 0, 0, 0, 0, 0, 0)));
+        assertEquals(2, winner(board(0, 0, 2, 0, 0, 2, 0, 0, 2)));
+        assertEquals(2, winner(board(0, 2, 0, 0, 2, 0, 0, 2, 0)));
+        assertEquals(2, winner(board(2, 0, 0, 2, 0, 0, 2, 0, 0)));
+        assertEquals(2, winner(board(2, 0, 0, 0, 2, 0, 0, 0, 2)));
+        assertEquals(2, winner(board(0, 0, 2, 0, 2, 0, 2, 0, 0)));
+        assertEquals(-1, winner(board(1, 1, 1, 0, 0, 0, 2, 2, 2)));
+    }
+    
+    @Test
+    public void testALL() {
+        short[][] b = ALL[6561];
+        System.out.println(Arrays.deepToString(b));
+        int count = 0;
+        for (short[][] e : ALL)
+            if (e != null) {
+                if (e[0] != null)
+                    ++count;
+                if (e[1] != null)
+                    ++count;
+            }
+        System.out.println("count=" + count);
+    }
 }

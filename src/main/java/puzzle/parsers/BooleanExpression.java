7dc2b839c63f2f975074b7aea84d0af4e8c1df99
saka1029 <saka1029@gmail.com>
2022-12-11 10:41:00 +0900

    puzzle.parsers.BooleanExpressionを更新した。

--- src/main/java/puzzle/parsers/BooleanExpression.java
+++ src/main/java/puzzle/parsers/BooleanExpression.java
@@ -1,92 +1,176 @@
 package puzzle.parsers;
 
-public class BooleanExpression {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+
+public interface BooleanExpression extends Predicate<Map<Character, Boolean>> {
+    static final BooleanExpression TRUE = context -> true;
+    static final BooleanExpression FALSE = context -> false;
+
+    static BooleanExpression variable(char name) {
+        return context -> context.get(name);
+    }
+
+    default BooleanExpression not() {
+        return context -> !test(context);
+    }
+
+    default BooleanExpression and(BooleanExpression right) {
+        return context -> test(context) && right.test(context);
+    }
+
+    default BooleanExpression or(BooleanExpression right) {
+        return context -> test(context) || right.test(context);
+    }
+
+    default BooleanExpression xor(BooleanExpression right) {
+        return context -> test(context) ^ right.test(context);
+    }
+
+    default BooleanExpression equals(BooleanExpression right) {
+        return context -> test(context) == right.test(context);
+    }
+
+    default BooleanExpression implies(BooleanExpression right) {
+        return context -> !test(context) || right.test(context);
+    }
+
+    public record ExpressionVariables(
+        BooleanExpression expression,
+        List<Character> variables) {
+    }
+
+    static boolean isVariable(int ch) {
+        return ch >= 'a' && ch <= 'z';
+    }
 
     /**
-     * expression = or
-     * or         = and { '||' and }
-     * and        = factor { '&&' factor }
-     * factor     = paren | comp
-     * paren      = '(' expression ')'
-     * comp       = number op number
-     * op         = '==' | '!=' | '<' | '<= | '>' | '>='
+     * <pre>
+     * [SYNTAX]
+     * expression = term { ( '=' | '->' ) term }
+     * term       = factor { ( '|' | '^' ) factor }
+     * factor     = primary { '&' primary }
+     * primary    = [ '!' ] ( 'T' | 'F' | variable | '(' expression ')' )
+     * variable   = 'a' | 'b' | ... | 'z'
+     * </pre>
      */
-    static boolean eval(String expression) {
+    public static ExpressionVariables parse(String source) {
         return new Object() {
-            int index = 0;
-            int ch = get();
+            int length = source.length(), index = 0, ch = get();
+            Set<Character> variables = new TreeSet<>();
 
             int get() {
-                return ch = index < expression.length() ? expression.charAt(index++) : -1;
+                return ch = index < length ? source.charAt(index++) : -1;
             }
 
-            void spaces() {
+            boolean eat(int expected) {
                 while (Character.isWhitespace(ch))
                     get();
-            }
-
-            boolean eat(String word) {
-                spaces();
-                if (ch != word.charAt(0) || !expression.startsWith(word.substring(1), index))
-                    return false;
-                index += word.length() - 1;
-                get();
-                return true;
-            }
-
-            int number() {
-                spaces();
-                if (ch != '-' && !Character.isDigit(ch))
-                    throw new RuntimeException(
-                        "number expected but '" + ((char)ch) + "'");
-                StringBuilder sb = new StringBuilder();
-                do {
-                    sb.append((char)ch);
+                if (ch == expected) {
                     get();
-                } while (Character.isDigit(ch));
-                return Integer.parseInt(sb.toString());
+                    return true;
+                }
+                return false;
             }
 
-            boolean paren() {
-                boolean result = or();
-                if (!eat(")")) throw new RuntimeException("')' expected");
-                return result;
-            }
-
-            boolean comp() {
-                if (eat("(")) return paren();
-                int term = number();
-                if (eat("==")) return term == number();
-                else if (eat("!=")) return term != number();
-                else if (eat("<=")) return term <= number();
-                else if (eat("<")) return term < number();
-                else if (eat(">=")) return term >= number();
-                else if (eat(">")) return term > number();
-                else throw new RuntimeException();
-            }
-
-            boolean and() {
-                boolean result = comp();
-                while (eat("&&"))
-                    result &= comp();
-                return result;
-            }
-
-            boolean or() {
-                boolean result = and();
-                while (eat("||"))
-                    result |= and();
-                return result;
-            }
-
-            boolean parse() {
-                boolean result = or();
-                if (ch != -1)
+            BooleanExpression primary() {
+                BooleanExpression e;
+                boolean not = eat('!');
+                if (eat('T')) {
+                    e = BooleanExpression.TRUE;
+                } else if (eat('F')) {
+                    e = BooleanExpression.FALSE;
+                } else if (isVariable(ch)) {
+                    char name = (char) ch;
+                    get();
+                    variables.add(name);
+                    e = BooleanExpression.variable(name);
+                } else if (eat('(')) {
+                    e = expression();
+                    if (!eat(')'))
+                        throw new RuntimeException("')' expected");
+                } else
                     throw new RuntimeException(
-                        "extra char '" + expression.substring(index - 1) + "'");
-                return result;
+                        "unknown char '" + (char) ch + "'");
+                if (not)
+                    e = e.not();
+                return e;
+            }
+
+            BooleanExpression factor() {
+                BooleanExpression e = primary();
+                while (eat('&'))
+                    e = e.and(primary());
+                return e;
+            }
+
+            BooleanExpression term() {
+                BooleanExpression e = factor();
+                while (true)
+                    if (eat('|'))
+                        e = e.or(factor());
+                    else if (eat('^'))
+                        e = e.xor(factor());
+                    else
+                        break;
+                return e;
+            }
+
+            BooleanExpression expression() {
+                BooleanExpression e = term();
+                while (true) {
+                    if (eat('='))
+                        e = e.equals(term());
+                    else if (eat('-')) {
+                        if (!eat('>'))
+                            throw new RuntimeException("'->' expected");
+                        e = e.implies(term());
+                    }
+                    break;
+                }
+                return e;
+            }
+
+            ExpressionVariables parse() {
+                return new ExpressionVariables(
+                    expression(),
+                    new ArrayList<>(variables));
             }
         }.parse();
     }
 
+    static final boolean[] BOOLEANS = {true, false};
+
+    public static void truthTable(String source) {
+        ExpressionVariables ev = parse(source);
+        int size = ev.variables().size();
+        Map<Character, Boolean> context = new HashMap<>();
+        for (char c : ev.variables())
+            System.out.print(c + " ");
+        System.out.println("| " + source);
+        System.out.println("--".repeat(size) + "+-" + "-".repeat(source.length()));
+        new Object() {
+
+            String toString(boolean value) {
+                return value ? "T " : "F ";
+            }
+
+            void gen(int index, String line) {
+                if (index >= size)
+                    System.out.println(line + "| "
+                        + toString(ev.expression().test(context)));
+                else
+                    for (boolean value : BOOLEANS) {
+                        context.put(ev.variables().get(index), value);
+                        gen(index + 1, line + toString(value));
+                    }
+            }
+        }.gen(0, "");
+        System.out.println();
+    }
 }
---
+++ src/test/java/test/puzzle/parsers/TestBooleanExpression.java
@@ -0,0 +1,46 @@
+package test.puzzle.parsers;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static puzzle.parsers.BooleanExpression.parse;
+import static puzzle.parsers.BooleanExpression.truthTable;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import puzzle.parsers.BooleanExpression;
+
+public class TestBooleanExpression {
+
+    @Test
+    public void testTruthTable() {
+        truthTable("T");
+        truthTable("F");
+        truthTable("!F");
+        truthTable("!a");
+        truthTable("a & b");
+        truthTable("a | b");
+        truthTable("a ^ b");
+        truthTable("a -> b");
+        truthTable("a = b");
+        truthTable("(a = b) = !(a ^ b)");
+        truthTable("(a -> b) = (!a | b)");
+    }
+
+    @Test
+    public void testNot() {
+        BooleanExpression e = parse("!a").expression();
+        assertFalse(e.test(Map.of('a', true)));
+        assertTrue(e.test(Map.of('a', false)));
+    }
+
+    @Test
+    public void testOr() {
+        BooleanExpression e = parse("a | b").expression();
+        assertTrue(e.test(Map.of('a', true, 'b', true)));
+        assertTrue(e.test(Map.of('a', true, 'b', false)));
+        assertTrue(e.test(Map.of('a', false, 'b', true)));
+        assertFalse(e.test(Map.of('a', false, 'b', false)));
+    }
+}

a601c3c022f26a6deb8a03eeb56e0f7720d4e076
saka1029 <saka1029@gmail.com>
2022-12-14 16:08:11 +0900

    Update TestPL0Parser.java

--- src/test/java/test/puzzle/language/TestPL0Parser.java
+++ src/test/java/test/puzzle/language/TestPL0Parser.java
@@ -8,232 +8,105 @@ import org.junit.Test;
 
 public class TestPL0Parser {
 
+    
+    /**
+     * <pre>
+     * 命令語 bit 構成
+     * alxxxxxx
+     * a bit = 0:アドレスあり 1:アドレスなし
+     * l bit = 0:レベルあり 1:レベルなし
+     * 4バイト命令 00xxxxxx AAAAAAAA AAAAAAAA LLLLLLLL
+     * 3バイト命令 01xxxxxx AAAAAAAA AAAAAAAA
+     * 1バイト命令 11xxxxxx
+     * </pre>
+     */
+    static final int LOD = 0x00, STO = 0x01, CAL = 0x02;
+    static final int LIT = 0x40, INC = 0x41, JMP = 0x42, JPC = 0x43;
+    static final int
+    OPRET = 0xC0, OPNEG = 0xC1, OPADD = 0xC2, OPSUB = 0xC3,
+        OPMUL = 0xC4, OPDIV = 0xC5, OPODD = 0xC6, OPINP = 0xC7,
+        OPOUT = 0xC8, OPCEQ = 0xC9, OPCNE = 0xCA, OPCLT = 0xCB,
+        OPCLE = 0xCC, OPCGT = 0xCD, OPCGE = 0xCE;
+    static final int ADDR_MASK = 0x80, LEVEL_MASK = 0x40;
+
+    static class Memory {
+        final byte[] memory;
+        int pointer = 0;
+
+        Memory(byte[] memory) {
+            this.memory = memory;
+        }
+
+        Memory(int size) {
+            this.memory = new byte[size];
+        }
+        
+        void set(int address, int value) {
+            memory[address] = (byte)(value & 0xff);
+        }
+        
+        void setInt(int address, int value) {
+            set(address, value >> 8);
+            set(address + 1, value);
+        }
+        
+        void add(int instruction) {
+            set(pointer++, instruction);
+        }
+
+        void add(int instruction, int level) {
+            add(instruction);
+            add(level);
+        }
+
+        void add(int instruction, int level, int operand) {
+            add(instruction, level);
+            setInt(pointer, operand);
+            pointer += 2;
+        }
+
+    }
+
     static final List<String> RESERVED = List.of(
         "const", "var", "procedure", "call", "begin", "end", "if", "then", "while", "do", "odd");
 
-    /**
-     * program = block "." .
-     * block      = [ "const" ident "=" number {"," ident "=" number} ";"]
-     *              [ "var" ident {"," ident} ";"]
-     *              { "procedure" ident ";" block ";" } statement .
-     * statement  = [ ident ":=" expression | "call" ident |
-     *              "begin" statement {";" statement } "end" |
-     *              "if" condition "then" statement |
-     *              "while" condition "do" statement ].
-     * condition  = "odd" expression |
-     *              expression ("="|"#"|"<"|"<="|">"|">=") expression .
-     * expression = [ "+"|"-"] term { ("+"|"-") term}.
-     * term       = factor {("*"|"/") factor}.
-     * factor     = ident | number | "(" expression ")".
-     */
-    public static void parse(String source) {
-        int length = source.length();
-        new Object() {
-            int index = 0;
-            String token;
-            
-            RuntimeException error(String format, Object... args) {
-                return new RuntimeException(format.formatted(args));
-            }
-
-            int ch() {
-                return index < length ? source.charAt(index) : -1;
-            }
-            
-            void spaces() {
-                while (Character.isWhitespace(ch()))
-                    ++index;
-            }
-
-            boolean eat(String... expects) {
-                spaces();
-                for (String expect : expects)
-                    if (source.startsWith(expect, index)) {
-                        token = expect;
-                        index += expect.length();
-                        return true;
-                    }
-                return false;
-            }
-            
-            boolean isNumber(int ch) {
-                return ch >= '0' && ch <= '9';
-            }
-            
-            boolean isIdFirst(int ch) {
-                return ch >= 'a' && ch <= 'z'
-                    || ch >= 'A' && ch <= 'Z';
-            }
-            
-            boolean isIdRest(int ch) {
-                return isIdFirst(ch) || isNumber(ch);
-            }
-
-            boolean eatWord(String expected) {
-                spaces();
-                if (!isIdFirst(ch()))
-                    return false;
-                int start = index;
-                do {
-                    ++index;
-                } while (isIdRest(ch()));
-                token = source.substring(start, index);
-                if (expected == null && !RESERVED.contains(token.toLowerCase()))
-                    return true;
-                if (expected != null && expected.equalsIgnoreCase(token))
-                    return true;
-                index = start;
-                return false;
-            }
-
-            boolean eatNumber() {
-                spaces();
-                if (!isNumber(ch()))
-                    return false;
-                int start = index;
-                do {
-                    ++index;
-                } while (isNumber(ch()));
-                token = source.substring(start, index);
-                return true;
-            }
-            
-            void factor() {
-                if (eat("(")) {
-                    expression();
-                    if (!eat(")"))
-                        throw error("')' expected");
-                } else if (eatNumber()) {
-                } else if (eatWord(null)) {
-                }
-            }
-
-            void term() {
-                factor();
-                while (eat("*", "/"))
-                    factor();
-            }
-            
-            void expression() {
-                if (eat("+", "-")) {
-                }
-                term();
-                while (eat("+", "-"))
-                    term();
-            }
-            
-            void condition() {
-                if (eat("odd")) {
-                    expression();
-                } else {
-                    expression();
-                    if (!eat("=", "#", "<=", ">=", "<", ">"))
-                        throw error("'=', '#', '<', '<=', '>' or '>=' expected");
-                    expression();
-                }
-            }
-            
-            void statement() {
-                if (eatWord(null)) {
-                    if (!eat(":="))
-                        throw error("':=' expected");
-                    expression();
-                } else if (eatWord("call")) {
-                    if (!eatWord(null))
-                        throw error("identifier expected");
-                } else if (eat("!")) {
-                    expression();
-                } else if (eat("?")) {
-                    if (!eatWord(null))
-                        throw error("identifier expected");
-                } else if (eatWord("begin")) {
-                    do {
-                        statement();
-                    } while (eat(";"));
-                    if (!eatWord("end"))
-                        throw error("'end' expected");
-                } else if (eatWord("if")) {
-                    condition();
-                    if (!eatWord("then"))
-                        throw error("'then' expected");
-                    statement();
-                } else if (eatWord("while")) {
-                    condition();
-                    if (!eatWord("do"))
-                        throw error("'do' expected");
-                    statement();
-                }
-            }
-
-            void block() {
-                if (eatWord("const")) {
-                    do {
-                        if (!eatWord(null))
-                            throw error("identifier expected");
-                        if (!eat("="))
-                            throw error("'=' expected");
-                        if (!eatNumber())
-                            throw error("number expected");
-                    } while (eat(","));
-                    if (!eat(";"))
-                        throw error("';' expected");
-                }
-                if (eatWord("var")) {
-                    do {
-                        if (!eatWord(null))
-                            throw error("identifier expected");
-                    } while (eat(","));
-                    if (!eat(";"))
-                        throw error("';' expected");
-                }
-                if (eatWord("procedure")) {
-                    if (!eatWord(null))
-                        throw error("identifier expected");
-                    if (!eat(";"))
-                        throw error("';' expected");
-                    block();
-                    if (!eat(";"))
-                        throw error("';' expected");
-                }
-                statement();
-            }
-
-            void program() {
-                block();
-                if (!eat("."))
-                    throw error("'.' expected");
-            }
-
-            void parse() {
-                program();
-            }
-        }.parse();
+    enum Type {
+        RESERVED, IDENT, NUMBER, END;
     }
-    
-    enum Type { RESERVED, IDENT, NUMBER, END; }
-    static void parserTokenize(String source) {
+
+    /**
+     * program = block "." . block = [ "const" ident "=" number {"," ident "="
+     * number} ";"] [ "var" ident {"," ident} ";"] { "procedure" ident ";" block ";"
+     * } statement . statement = [ ident ":=" expression | "call" ident | "begin"
+     * statement {";" statement } "end" | "if" condition "then" statement | "while"
+     * condition "do" statement ]. condition = "odd" expression | expression
+     * ("="|"#"|"<"|"<="|">"|">=") expression . expression = [ "+"|"-"] term {
+     * ("+"|"-") term}. term = factor {("*"|"/") factor}. factor = ident | number |
+     * "(" expression ")".
+     */
+    static void parse(String source) {
         new Object() {
             int length = source.length(), index = 0;
             Type type;
             String nextToken, token;
-            
+
             RuntimeException error(String format, Object... args) {
                 return new RuntimeException(format.formatted(args));
             }
-            
+
             int ch() {
                 return index < length ? source.charAt(index) : -1;
             }
-            
+
             boolean isNumber(int ch) {
                 return ch >= '0' && ch <= '9';
             }
-            
+
             boolean isIdFirst(int ch) {
                 return ch >= 'a' && ch <= 'z'
                     || ch >= 'A' && ch <= 'Z';
             }
-            
+
             boolean isIdRest(int ch) {
                 return isIdFirst(ch) || isNumber(ch);
             }
@@ -247,15 +120,27 @@ public class TestPL0Parser {
                         type = Type.END;
                         nextToken = null;
                         break;
-                    case '(': case ')': case ';': case ',': case '.':
-                    case '?': case '!': case '=': case '#':
-                    case '+': case '-': case '*': case '/':
+                    case '(':
+                    case ')':
+                    case ';':
+                    case ',':
+                    case '.':
+                    case '?':
+                    case '!':
+                    case '=':
+                    case '#':
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
                         type = Type.RESERVED;
                         nextToken = Character.toString(ch());
                         ++index;
                         break;
-                    case '<': case '>':  case ':':
-                        char f = (char)ch();
+                    case '<':
+                    case '>':
+                    case ':':
+                        char f = (char) ch();
                         ++index;
                         type = Type.RESERVED;
                         if (ch() == '=') {
@@ -281,7 +166,7 @@ public class TestPL0Parser {
                         break;
                 }
             }
-            
+
             boolean eat(String... expects) {
                 for (String expect : expects)
                     if (nextToken.equalsIgnoreCase(expect)) {
@@ -291,7 +176,7 @@ public class TestPL0Parser {
                     }
                 return false;
             }
-            
+
             boolean eat(Type expect) {
                 if (expect == type) {
                     token = nextToken;
@@ -300,17 +185,17 @@ public class TestPL0Parser {
                 }
                 return false;
             }
-            
+
             void check(String message, String... expects) {
                 if (!eat(expects))
                     throw error(message);
             }
-            
+
             void check(String message, Type type) {
                 if (!eat(type))
                     throw error(message);
             }
-            
+
             void factor() {
                 if (eat(Type.IDENT)) {
                 } else if (eat(Type.NUMBER)) {
@@ -379,7 +264,7 @@ public class TestPL0Parser {
                         check("number expected", Type.NUMBER);
                     } while (eat(","));
                     check("';' expected", ";");
-                } 
+                }
                 if (eat("var")) {
                     do {
                         check("ident expected", Type.IDENT);
@@ -427,27 +312,6 @@ public class TestPL0Parser {
             + "   END\r\n"
             + "END.";
         parse(source);
-    }
-
-    @Test
-    public void testParseTokenize() {
-        String source = "VAR x, squ;\r\n"
-            + "\r\n"
-            + "PROCEDURE square;\r\n"
-            + "BEGIN\r\n"
-            + "   squ := x * x\r\n"
-            + "END;\r\n"
-            + "\r\n"
-            + "BEGIN\r\n"
-            + "   x := 1;\r\n"
-            + "   WHILE x <= 10 DO\r\n"
-            + "   BEGIN\r\n"
-            + "      CALL square;\r\n"
-            + "      ! squ;\r\n"
-            + "      x := x + 1;\r\n"
-            + "   END\r\n"
-            + "END.";
-        parserTokenize(source);
     }
 
 }

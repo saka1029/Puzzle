b4a4481b836bf513573dfe72f50f8e5fe92e0e91
saka1029 <saka1029@gmail.com>
2022-12-24 14:52:35 +0900

    Create TestSudoku.java

---
+++ src/test/java/test/puzzle/core/TestSudoku.java
@@ -0,0 +1,103 @@
+package test.puzzle.core;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class TestSudoku {
+
+    static boolean solve(int[][] board) {
+        int size = 9;
+        return new Object() {
+            
+            boolean isNumberInRow(int number, int row) {
+                for (int i = 0; i < size; ++i)
+                    if (board[row][i] == number)
+                        return true;
+                return false;
+            }
+            
+            boolean isNumberInColumn(int number, int column) {
+                for (int i = 0; i < size; ++i)
+                    if (board[i][column] == number)
+                        return true;
+                return false;
+            }
+            
+            boolean isNumberInBox(int number, int row, int column) {
+                int boxRow = row - row % 3;
+                int boxColumn = column - column % 3;
+                for (int i = boxRow, maxRow = boxRow + 3; i < maxRow; ++i)
+                    for (int j = boxColumn, maxColumn = boxColumn + 3; j < maxColumn; ++j)
+                        if (board[i][j] == number)
+                            return true;
+                return false;
+            }
+            
+            boolean isValidPlacement(int number, int row, int column) {
+                return !isNumberInRow(number, row)
+                    && !isNumberInColumn(number, column)
+                    && !isNumberInBox(number, row, column);
+            }
+            
+            boolean solve() {
+                for (int row = 0; row < size; ++row) {
+                    for (int column = 0; column < size; ++column) {
+                        if (board[row][column] == 0) {
+                            for (int number = 1; number <= 9; ++number) {
+                                if (isValidPlacement(number, row, column)) {
+                                    board[row][column] = number;
+                                    if (solve())
+                                        return true;
+                                    else
+                                        board[row][column] = 0;
+                                }
+                            }
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }.solve();
+    }
+
+    @Test
+    public void test() {
+        int[][] board = {
+            {7, 0, 2, 0, 5, 0, 6, 0, 0},
+            {0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {1, 0, 0, 0, 0, 9, 5, 0, 0},
+            {8, 0, 0, 0, 0, 0, 0, 9, 0},
+            {0, 4, 3, 0, 0, 0, 7, 5, 0},
+            {0, 9, 0, 0, 0, 0, 0, 0, 8},
+            {0, 0, 9, 7, 0, 0, 0, 0, 5},
+            {0, 0, 0, 2, 0, 0, 0, 0, 0},
+            {0, 0, 7, 0, 4, 0, 2, 0, 3},
+        };
+        if (solve(board))
+            for (int[] row : board)
+                System.out.println(Arrays.toString(row));
+    }
+
+    @Test
+    public void testZeros() {
+        int[][] board = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
+        if (solve(board))
+            for (int[] row : board)
+                System.out.println(Arrays.toString(row));
+    }
+
+}

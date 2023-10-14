4d3d7a71c9440a7b6c965f5beb6160b48169b74c
saka1029 <saka1029@gmail.com>
2023-10-14 17:54:54 +0900

    Update TestForthBlock.java

--- src/test/java/test/puzzle/core/TestForthBlock.java
+++ src/test/java/test/puzzle/core/TestForthBlock.java
@@ -1,6 +1,7 @@
 package test.puzzle.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;

664b7d87099515759cd936c52694c319a3280f02
saka1029 <saka1029@gmail.com>
2024-02-15 17:48:09 +0900

    Update TestSequenciable.java

--- src/test/java/test/puzzle/core/TestSequenciable.java
+++ src/test/java/test/puzzle/core/TestSequenciable.java
@@ -24,4 +24,15 @@ public class TestSequenciable {
     public void testFilter() {
         assertEquals(seq(2, 4, 6), filter(i -> i % 2 == 0, seq(1, 2, 3, 4, 5, 6)));
     }
+    
+    @Test
+    public void test10000() {
+        int count = 0;
+        for (int i = 1; i <= 10000; ++i)
+            if (i % ((int)Math.sqrt(i)) == 0) {
+                System.out.print(" " + i);
+                ++count;
+            }
+        System.out.println("count=" + count);
+    }
 }

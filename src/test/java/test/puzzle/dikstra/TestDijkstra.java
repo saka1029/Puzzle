7244bd254452da0e8b441fe545bf05cbabddedd7
saka1029 <saka1029@gmail.com>
2025-04-13 19:44:38 +0900

    TestDijkstra

--- src/test/java/test/puzzle/dikstra/TestDijkstra.java
+++ src/test/java/test/puzzle/dikstra/TestDijkstra.java
@@ -7,42 +7,42 @@ import org.junit.Test;
 
 public class TestDijkstra {
 
-    enum 駅 { 東京, 渋谷, 品川, 新横浜, 小田原, 新宿, 横浜 }
+    enum Eki { 東京, 渋谷, 品川, 新横浜, 小田原, 新宿, 横浜 }
 
     @Test
     @SuppressWarnings("unused")
     public void test() {
-        駅[] all = 駅.values();
+        Eki[] all = Eki.values();
         int size = all.length;
         int[][] 所要時間 = new int[size][size];
-        駅[] 経路 = new 駅[size];
+        Eki[] 経路 = new Eki[size];
         int[] 最短時間 = new int[size];
-        EnumSet<駅> 訪問済 = EnumSet.noneOf(駅.class);
+        EnumSet<Eki> 訪問済 = EnumSet.noneOf(Eki.class);
         new Object() {
 
-            void set(駅 from, 駅 to, int 時間) {
+            void set(Eki from, Eki to, int 時間) {
                 所要時間[from.ordinal()][to.ordinal()] = 時間;
                 所要時間[to.ordinal()][from.ordinal()] = 時間;
             }
 
             void init() {
-                for (駅 f : all)
-                    for (駅 t : all)
+                for (Eki f : all)
+                    for (Eki t : all)
                         set(f, t, -1);
-                set(駅.東京, 駅.渋谷, 16);
-                set(駅.東京, 駅.品川, 8);
-                set(駅.東京, 駅.新宿, 15);
-                set(駅.渋谷, 駅.品川, 13);
-                set(駅.渋谷, 駅.新横浜, 27);
-                set(駅.渋谷, 駅.小田原, 78);
-                set(駅.品川, 駅.新横浜, 10);
-                set(駅.品川, 駅.新宿, 21);
-                set(駅.品川, 駅.横浜, 17);
-                set(駅.新横浜, 駅.横浜, 13);
-                set(駅.新横浜, 駅.小田原, 15);
-                set(駅.新宿, 駅.横浜, 33);
-                set(駅.横浜, 駅.小田原, 52);
-                最短時間[駅.東京.ordinal()] = 0;
+                set(Eki.東京, Eki.渋谷, 16);
+                set(Eki.東京, Eki.品川, 8);
+                set(Eki.東京, Eki.新宿, 15);
+                set(Eki.渋谷, Eki.品川, 13);
+                set(Eki.渋谷, Eki.新横浜, 27);
+                set(Eki.渋谷, Eki.小田原, 78);
+                set(Eki.品川, Eki.新横浜, 10);
+                set(Eki.品川, Eki.新宿, 21);
+                set(Eki.品川, Eki.横浜, 17);
+                set(Eki.新横浜, Eki.横浜, 13);
+                set(Eki.新横浜, Eki.小田原, 15);
+                set(Eki.新宿, Eki.横浜, 33);
+                set(Eki.横浜, Eki.小田原, 52);
+                最短時間[Eki.東京.ordinal()] = 0;
             }
 
             void solve() {

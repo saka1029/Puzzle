08bb0a4e3618c48cf0444912f07f1ca5d3d8c5c6
saka1029 <saka1029@gmail.com>
2025-04-21 20:09:24 +0900

    nanopico trans, rot

--- src/test/java/test/puzzle/nanopico/Test_002_MatrixKaiten.java
+++ src/test/java/test/puzzle/nanopico/Test_002_MatrixKaiten.java
@@ -1,10 +1,92 @@
 package test.puzzle.nanopico;
 
+import static org.junit.Assert.assertArrayEquals;
 import java.util.function.Function;
 
 import org.junit.Test;
 
 public class Test_002_MatrixKaiten {
+
+    public static void trans(int[][] matrix) {
+        int n = matrix.length;
+        for (int r = 0; r < n; ++r)
+            for (int c = r + 1; c < n; ++c) {
+                int temp = matrix[c][r];
+                matrix[c][r] = matrix[r][c];
+                matrix[r][c] = temp;
+            }
+    }
+
+    @Test
+    public void testTrans() {
+        int[][] m3 = new int[][] {
+            {00, 01, 02},
+            {10, 11, 12},
+            {20, 21, 22},
+        };
+        trans(m3);
+        assertArrayEquals(new int[][] {
+            {00, 10, 20},
+            {01, 11, 21},
+            {02, 12, 22},
+        }, m3);
+        int[][] m4 = new int[][] {
+            {00, 01, 02, 03},
+            {10, 11, 12, 13},
+            {20, 21, 22, 23},
+            {30, 31, 32, 33},
+        };
+        trans(m4);
+        assertArrayEquals(new int[][] {
+            {00, 10, 20, 30},
+            {01, 11, 21, 31},
+            {02, 12, 22, 32},
+            {03, 13, 23, 33},
+        }, m4);
+    }
+
+    public static void rot(int[][] matrix) {
+        int n = matrix.length, rmax = (n + 1) / 2, cmax = n / 2;
+        for (int r = 0; r < rmax; ++r) {
+            int rr = n - r - 1;
+            for (int c = 0; c < cmax; ++c) {
+                int cc = n - c - 1;
+                int temp = matrix[r][c];
+                matrix[r][c] = matrix[c][rr];
+                matrix[c][rr] = matrix[rr][cc];
+                matrix[rr][cc] = matrix[cc][r];
+                matrix[cc][r] = temp;
+            }
+        }
+    }
+
+    @Test
+    public void testRot() {
+        int[][] m3 = new int[][] {
+            {00, 01, 02},
+            {10, 11, 12},
+            {20, 21, 22},
+        };
+        rot(m3);
+        assertArrayEquals(new int[][] {
+            {02, 12, 22},
+            {01, 11, 21},
+            {00, 10, 20},
+        }, m3);
+        int[][] m4 = new int[][] {
+            {00, 01, 02, 03},
+            {10, 11, 12, 13},
+            {20, 21, 22, 23},
+            {30, 31, 32, 33},
+        };
+        rot(m4);
+        assertArrayEquals(new int[][] {
+            {03, 13, 23, 33},
+            {02, 12, 22, 32},
+            {01, 11, 21, 31},
+            {00, 10, 20, 30},
+        }, m4);
+    }
 
     static final int N = 9;
     record P(int r, int c) {}

c19a0e0afbc24a03448524129a027a88e4d3afd0
saka1029 <saka1029@gmail.com>
2025-04-23 05:51:24 +0900

    spiral

--- src/test/java/test/puzzle/core/TestMatrixSpiral.java
+++ src/test/java/test/puzzle/core/TestMatrixSpiral.java
@@ -10,20 +10,17 @@ import org.junit.Test;
 
 public class TestMatrixSpiral {
 
+    static boolean add(List<Integer> list, int value) {
+        return value > 0 ? list.add(value) : false;
+    }
+
     static int[] steps(int rows, int cols) {
         List<Integer> r = new ArrayList<>();
-        --rows;
-        --cols;
-        r.add(cols);
-        while (true) {
-            if (rows <= 0) break;
-            r.add(rows--);
-            if (cols <= 0) break;
-            r.add(cols--);
-        }
-        return r.stream()
-            .mapToInt(i -> i)
-            .toArray();
+        --rows; --cols;
+        add(r, cols);
+        while (add(r, rows--) && add(r, cols--))
+            /* do nothing */;
+        return r.stream().mapToInt(i -> i).toArray();
     }
 
     void testSteps(int[] expected, int rows, int cols) {
@@ -38,8 +35,48 @@ public class TestMatrixSpiral {
         testSteps(new int[] {1, 1, 1}, 2, 2);
         testSteps(new int[] {2, 2, 2, 1, 1}, 3, 3);
         testSteps(new int[] {3, 3, 3, 2, 2, 1, 1}, 4, 4);
+        testSteps(new int[] {3}, 4, 1);
+        testSteps(new int[] {3}, 1, 4);
         testSteps(new int[] {4, 2, 4, 1, 3}, 3, 5);
         testSteps(new int[] {2, 4, 2, 3, 1, 2}, 5, 3);
+    }
+
+    int[][] spiral(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        int[] steps = steps(rows, cols);
+        int sl = steps.length;
+        int x = 0, y = 0;
+        int dx = 0, dy = 1;
+        int n = 0;
+        // matrix[x][y] = n++;
+        for (int i = 0; i < sl; ++i) {
+            int sm = steps[i];
+            for (int j = 0; j < sm; ++j) {
+                matrix[x][y] = n++;
+                x += dx;
+                y += dy;
+            }
+            int dt = dx;
+            dx = dy;
+            dy = -dt;
+        }
+        matrix[x][y] = n++;
+        return matrix;
+    }
+
+    static void print(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length;
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c)
+                System.out.printf("%3d", matrix[r][c]);
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void testSpiral() {
+        int[][] m = spiral(6, 6);
+        print(m);
     }
 
 }

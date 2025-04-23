7e8cccda2b95ca45e2c986b566b13d470a9155a0
saka1029 <saka1029@gmail.com>
2025-04-23 22:17:16 +0900

    ..

--- src/test/java/test/puzzle/core/TestMatrixSpiral.java
+++ src/test/java/test/puzzle/core/TestMatrixSpiral.java
@@ -3,7 +3,6 @@ package test.puzzle.core;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.Test;
@@ -16,16 +15,17 @@ public class TestMatrixSpiral {
 
     static int[] steps(int rows, int cols) {
         List<Integer> r = new ArrayList<>();
-        --rows; --cols;
+        --rows;
+        --cols;
         add(r, cols);
         while (add(r, rows--) && add(r, cols--))
-            /* do nothing */;
+        /* do nothing */;
         return r.stream().mapToInt(i -> i).toArray();
     }
 
     void testSteps(int[] expected, int rows, int cols) {
         int[] steps = steps(rows, cols);
-        System.out.printf("rows=%d cols=%d steps=%s%n", rows, cols, Arrays.toString(steps));
+        // System.out.printf("rows=%d cols=%d steps=%s%n", rows, cols, Arrays.toString(steps));
         assertArrayEquals(expected, steps);
         assertEquals(IntStream.of(steps).sum(), rows * cols - 1);
     }
@@ -44,7 +44,8 @@ public class TestMatrixSpiral {
     int[][] spiral(int rows, int cols) {
         int[][] matrix = new int[rows][cols];
         int x = 0, y = 0, n = 0;
-        for (int i = 0, dx = 0, dy = 1, d = 0, s[] = steps(rows, cols); i < s.length; ++i, d = dx, dx = dy, dy = -d)
+        for (int i = 0, dx = 0, dy = 1, d = 0, s[] = steps(rows, cols); i < s.length; ++i, d =
+                dx, dx = dy, dy = -d)
             for (int j = 0; j < s[i]; ++j, x += dx, y += dy)
                 matrix[x][y] = n++;
         matrix[x][y] = n++;
@@ -62,8 +63,17 @@ public class TestMatrixSpiral {
 
     @Test
     public void testSpiral() {
-        int[][] m = spiral(9, 6);
-        print(m);
+        assertArrayEquals(new int[][] {
+            {0, 1, 2, 3, 4, 5},
+            {25, 26, 27, 28, 29, 6},
+            {24, 43, 44, 45, 30, 7},
+            {23, 42, 53, 46, 31, 8},
+            {22, 41, 52, 47, 32, 9},
+            {21, 40, 51, 48, 33, 10},
+            {20, 39, 50, 49, 34, 11},
+            {19, 38, 37, 36, 35, 12},
+            {18, 17, 16, 15, 14, 13},
+        }, spiral(9, 6));
     }
 
     static class Spiral {
@@ -95,12 +105,12 @@ public class TestMatrixSpiral {
             int rows = right ? rl : cl, cols = right ? cl : rl;
             forward(cols);
             while (forward(rows--) && forward(cols--))
-                /* do nothing */;
+            /* do nothing */;
             matrix[x][y] = n;
         }
 
         static int[][] of(int rows, int cols, boolean right) {
-            Spiral spiral =  new Spiral(rows, cols);
+            Spiral spiral = new Spiral(rows, cols);
             spiral.walk(right);
             // print(spiral.matrix);
             // System.out.println();
@@ -110,32 +120,16 @@ public class TestMatrixSpiral {
 
     @Test
     public void testSpiralRight() {
-        assertArrayEquals(new int[][] {
-            {0, 1, 2},
-            {7, 8, 3},
-            {6, 5, 4},
-        }, Spiral.of(3, 3, true));
-        assertArrayEquals(new int[][] {
-            {0, 1, 2},
-            {9, 10, 3},
-            {8, 11, 4},
-            {7, 6, 5},
-        }, Spiral.of(4, 3, true));
+        assertArrayEquals(new int[][] {{0, 1, 2}, {7, 8, 3}, {6, 5, 4},}, Spiral.of(3, 3, true));
+        assertArrayEquals(new int[][] {{0, 1, 2}, {9, 10, 3}, {8, 11, 4}, {7, 6, 5},},
+                Spiral.of(4, 3, true));
     }
 
     @Test
     public void testSpiralLeft() {
-        assertArrayEquals(new int[][] {
-            {0, 7, 6},
-            {1, 8, 5},
-            {2, 3, 4},
-        }, Spiral.of(3, 3, false));
-        assertArrayEquals(new int[][] {
-            {0, 9, 8},
-            {1, 10, 7},
-            {2, 11, 6},
-            {3, 4, 5},
-        }, Spiral.of(4, 3, false));
+        assertArrayEquals(new int[][] {{0, 7, 6}, {1, 8, 5}, {2, 3, 4},}, Spiral.of(3, 3, false));
+        assertArrayEquals(new int[][] {{0, 9, 8}, {1, 10, 7}, {2, 11, 6}, {3, 4, 5},},
+                Spiral.of(4, 3, false));
     }
 
 }

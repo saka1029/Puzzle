ea974bc72a67e8f3b7a971afe867819410f9a967
saka1029 <saka1029@gmail.com>
2025-04-23 16:36:16 +0900

    ..

--- src/test/java/test/puzzle/core/TestMatrixSpiral.java
+++ src/test/java/test/puzzle/core/TestMatrixSpiral.java
@@ -68,7 +68,16 @@ public class TestMatrixSpiral {
 
     static class Spiral {
         final int[][] matrix;
-        int n = 0, x = 0, y = 0, dx = 0, dy = 1;
+        final int rows, cols;
+        int n = 0, x = 0, y = 0;
+        int dx, dy;
+        boolean right = true;
+
+        public Spiral(int rows, int cols) {
+            this.rows = rows;
+            this.cols = cols;
+            this.matrix = new int[rows][cols];
+        }
 
         boolean forward(int s) {
             if (s <= 0)
@@ -76,37 +85,71 @@ public class TestMatrixSpiral {
             for (int i = 0; i < s; ++i, x += dx, y += dy)
                 matrix[x][y] = n++;
             int d = dx;
-            dx = dy;
-            dy = -d;
+            dx = right ? dy : -dy;
+            dy = right ? -d : d;
             return true;
         }
 
-        Spiral(int rows, int cols) {
-            this.matrix = new int[rows--][cols--];
+        void right() {
+            right = true;
+            dx = 0; dy = 1;
+            int rows = matrix.length - 1, cols = matrix[0].length - 1;
             forward(cols);
             while (forward(rows--) && forward(cols--))
                 /* do nothing */;
             matrix[x][y] = n;
         }
 
-        static int[][] right(int rows, int cols) {
-            return new Spiral(rows, cols).matrix;
+        void left() {
+            right = false;
+            dx = 1; dy = 0;
+            int rows = matrix.length - 1, cols = matrix[0].length - 1;
+            forward(rows);
+            while (forward(cols--) && forward(rows--))
+                /* do nothing */;
+            matrix[x][y] = n;
+        }
+
+        static int[][] of(int rows, int cols, boolean right) {
+            Spiral spiral =  new Spiral(rows, cols);
+            if (right)
+                spiral.right();
+            else
+                spiral.left();
+            print(spiral.matrix);
+            System.out.println();
+            return spiral.matrix;
         }
     }
 
     @Test
-    public void testSpiralClass() {
+    public void testSpiralRight() {
         assertArrayEquals(new int[][] {
             {0, 1, 2},
             {7, 8, 3},
             {6, 5, 4},
-        }, Spiral.right(3, 3));
+        }, Spiral.of(3, 3, true));
         assertArrayEquals(new int[][] {
             {0, 1, 2},
             {9, 10, 3},
             {8, 11, 4},
             {7, 6, 5},
-        }, Spiral.right(4, 3));
+        }, Spiral.of(4, 3, true));
+    }
+
+    @Test
+    public void testSpiralLeft() {
+        assertArrayEquals(new int[][] {
+            {0, 7, 6},
+            {1, 8, 5},
+            {2, 3, 4},
+        }, Spiral.of(3, 3, false));
+        assertArrayEquals(new int[][] {
+            {0, 9, 8},
+            {1, 10, 7},
+            {2, 11, 6},
+            {3, 4, 5},
+        }, Spiral.of(4, 3, false));
     }
 
 }

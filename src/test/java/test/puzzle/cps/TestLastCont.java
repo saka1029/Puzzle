a7ea9931b601f66cec1f9a17f358812f26972d15
saka1029 <saka1029@gmail.com>
2025-10-06 21:36:50 +0900

    testFibonacci

--- src/test/java/test/puzzle/cps/TestLastCont.java
+++ src/test/java/test/puzzle/cps/TestLastCont.java
@@ -29,4 +29,32 @@ public class TestLastCont {
         f.factorial(2, c); assertEquals(2, r[0]);
         f.factorial(3, c); assertEquals(6, r[0]);
     }
+
+    @Test
+    public void testFibonacci() {
+        var f = new Object() {
+            void add(int a, int b, Cont c) { c.apply(a + b); }
+            void subtract(int a, int b, Cont c) { c.apply(a - b); }
+            void fibonacci(int n, Cont c) {
+                if (n <= 1)
+                    c.apply(n);
+                else
+                    subtract(n, 1, n1 ->
+                        fibonacci(n1, f1 ->
+                            subtract(n, 2, n2 ->
+                                fibonacci(n2, f2 ->
+                                    add(f1, f2, c)))));
+            }
+            int result = 0;
+            void save(int i) { result = i; }
+        };
+        f.fibonacci(0, f::save); assertEquals(0, f.result);
+        f.fibonacci(1, f::save); assertEquals(1, f.result);
+        f.fibonacci(2, f::save); assertEquals(1, f.result);
+        f.fibonacci(3, f::save); assertEquals(2, f.result);
+        f.fibonacci(4, f::save); assertEquals(3, f.result);
+        f.fibonacci(5, f::save); assertEquals(5, f.result);
+        f.fibonacci(6, f::save); assertEquals(8, f.result);
+        f.fibonacci(7, f::save); assertEquals(13, f.result);
+    }
 }

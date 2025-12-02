6b97370eab3e18212fac300866cc73f88cd33303
saka1029 <saka1029@gmail.com>
2025-12-02 21:21:43 +0900

    ..

--- src/test/javascript/puzzle/read_test.js
+++ src/test/javascript/puzzle/read_test.js
@@ -7,20 +7,23 @@ const reader = readline.createInterface({ input: stream });
 const trie = new Trie();
 let count = 0;
 function remq(s) {
-    return s.substring(1, s.length - 2);
+    return s.substring(1, s.length - 1);
 }
 reader.on('line', (line) => {
     // console.log(line);
-    if (count % 200 == 0) {
+    // if (count % 200 == 0) {
+    if (true) {
         const items = line.split(",");
         const code = remq(items[2]);
         const name = remq(items[5]);
-        console.log(`${count}:${code}:${name}`)
-        trie.insert(name, code);
+        if (code != "0000999") {
+            // console.log(`${count}:${code}:${name}`)
+            trie.insert(name, code);
+        }
     }
     ++count;
 });
 reader.on('close', () => {
     console.log(`count=${count}`);
-    console.log(trie.toString());
+    // console.log(trie.toString());
 });

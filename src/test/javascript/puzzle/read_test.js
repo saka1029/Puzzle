9a3055dc831cc7f0ac77072e643ead62d05b102a
saka1029 <saka1029@gmail.com>
2025-12-02 19:25:25 +0900

    ..

--- src/test/javascript/puzzle/read_test.js
+++ src/test/javascript/puzzle/read_test.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
+const Trie = require("./trie");
 const readline = require('readline');
 
 const stream = fs.createReadStream('data/b_20250601.txt', 'utf8');
 const reader = readline.createInterface({ input: stream });
+const trie = new Trie();
 let count = 0;
 function remq(s) {
     return s.substring(1, s.length - 2);
 }
 reader.on('line', (line) => {
     // console.log(line);
-    if (count % 100 == 0) {
+    if (count % 200 == 0) {
         const items = line.split(",");
-        console.log(`${count}:${remq(items[2])}:${remq(items[5])}`)
+        const code = remq(items[2]);
+        const name = remq(items[5]);
+        console.log(`${count}:${code}:${name}`)
+        trie.insert(name, code);
     }
     ++count;
 });
 reader.on('close', () => {
     console.log(`count=${count}`);
+    console.log(trie.toString());
 });

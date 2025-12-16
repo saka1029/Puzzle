bb484daa64ef7392ca5791e3fc201122c9667ffa
saka1029 <saka1029@gmail.com>
2025-12-17 05:53:00 +0900

    ..

--- src/main/java/puzzle/encoder/Encoder.java
+++ src/main/java/puzzle/encoder/Encoder.java
@@ -5,9 +5,29 @@ import java.util.function.Predicate;
 
 public interface Encoder<V> { 
 
-    List<List<EncoderEntry<V>>> encode(String s, Predicate<List<EncoderEntry<V>>> filter);
+    public static class Entry<V> {
+        public final String key;
+        public final V value;
 
-    default List<List<EncoderEntry<V>>> encode(String s) {
+        Entry(String key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public static <V> Entry<V> of(String key, V value) {
+            return new Entry<V>(key, value);
+        }
+
+        @Override
+        public String toString() {
+            return "Entry(%s, %s)".formatted(key, value);
+        }
+    }
+ 
+
+    List<List<Entry<V>>> encode(String s, Predicate<List<Entry<V>>> filter);
+
+    default List<List<Entry<V>>> encode(String s) {
         return encode(s, e -> true);
     }
 }

d86b8ea4e58afe2de49b4b66b25a29880db3379b
saka1029 <saka1029@gmail.com>
2025-12-17 23:12:56 +0900

    ..

--- src/main/java/puzzle/encoder/HashEncoder.java
+++ src/main/java/puzzle/encoder/HashEncoder.java
@@ -15,34 +15,10 @@ public class HashEncoder<V> implements Encoder<V>{
         map.put(key, value);
     }
 
-    // function encode(text) {
-    //     const length = text.length;
-    //     const result = [];
-    //     const sequence = [];
-
-    //     function search(start) {
-    //         if (start >= length) {
-    //             if (sequence.filter(e => e.data.length == 7).length == 1)
-    //                 result.push(sequence.slice());
-    //         } else
-    //             for (let i = start + 1; i <= length; ++i) {
-    //                 const key = text.substring(start, i);
-    //                 const data = BYOMEI[key];
-    //                 // console.log(`start=${start}, i=${i}, key=${key}, data=${data}`)
-    //                 if (data === undefined)
-    //                     continue;
-    //                 sequence.push({key: key, data: data});
-    //                 search(i);
-    //                 sequence.pop();
-    //             }
-    //     }
-    //     search(0);
-    //     return result;
-    // }
-
     @Override
     public List<List<Entry<V>>> encode(String text, Predicate<List<Entry<V>>> filter) {
-        int length = text.length();
+        int[] textcp = text.codePoints().toArray();
+        int length = textcp.length;
         List<List<Entry<V>>> result = new ArrayList<>();
         List<Entry<V>> sequence = new ArrayList<>();
         new Object() {
@@ -52,7 +28,7 @@ public class HashEncoder<V> implements Encoder<V>{
                         result.add(new ArrayList<>(sequence));
                 } else {
                     for (int i = start + 1; i <= length; ++i) {
-                        String key = text.substring(start, i);
+                        String key = new String(textcp, start, i - start);
                         V value = map.get(key);
                         if (value == null)
                             continue;

90e51c684234aacf3638be8044296760379f692a
saka1029 <saka1029@gmail.com>
2025-12-20 05:31:33 +0900

    Sudoku.java

---
+++ src/main/java/puzzle/core/Sudoku.java
@@ -0,0 +1,99 @@
+package puzzle.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * bitmapを使って適用可能な番号のみを順次取り出すようにし、
+ * 適用不能な番号のチェックを行わずに済むようにした。
+ * つまり、for (int n = 1; n <= 9; ++n)のループを高速化した。
+ * 終盤になると適用不能な番号が増えてくるので、
+ * そのチェックをスキップできると高速化できる。
+ * xxxSet[]は使用済み番号の集合の配列。
+ * 各要素は番号nが使用済みのとき、2ⁿビットがONになっている。
+ * nは1から9の数字なので、全ての番号が使用済みの時は0b111_111_111_0となる。
+ */
+public class Sudoku {
+
+    static final int SIZE = 9, MASK = 0b111_111_111_0;
+    final int[][] a;
+    final int[] rowSet = new int[SIZE], colSet = new int[SIZE], boxSet = new int[SIZE];
+    final List<int[][]> result = new ArrayList<>();
+
+    private Sudoku(int[][] a) {
+        this.a = a;
+        // 配列a初期化：既に確定している番号をbitmapにセットする。
+        for (int r = 0; r < SIZE; ++r)
+            for (int c = 0; c < SIZE; ++c) {
+                int n = a[r][c];
+                if (n != 0)
+                    set(r, c, 1 << n);
+            }
+    }
+
+    void set(int r, int c, int bit) {
+        rowSet[r] |= bit;
+        colSet[c] |= bit;
+        boxSet[box(r, c)] |= bit;
+        a[r][c] = Integer.numberOfTrailingZeros(bit);
+    }
+
+    void unset(int r, int c, int bit) {
+        rowSet[r] ^= bit;
+        colSet[c] ^= bit;
+        boxSet[box(r, c)] ^= bit;
+        a[r][c] = 0;
+    }
+
+    void answer() {
+        result.add(Stream.of(a)
+            .map(x -> Arrays.copyOf(x, x.length))
+            .toArray(int[][]::new));
+    }
+    
+    /**
+     * r行c列が属するbox(3x3)のセル位置を求める。
+     * <pre>
+     * \ c 0 1 2 3 4 5 6 7 8
+     * r +------------------
+     * 0 | 0 0 0 1 1 1 2 2 2 
+     * 1 | 0 0 0 1 1 1 2 2 2 
+     * 2 | 0 0 0 1 1 1 2 2 2 
+     * 3 | 3 3 3 4 4 4 5 5 5 
+     * 4 | 3 3 3 4 4 4 5 5 5 
+     * 5 | 3 3 3 4 4 4 5 5 5 
+     * 6 | 6 6 6 7 7 7 8 8 8 
+     * 7 | 6 6 6 7 7 7 8 8 8 
+     * 8 | 6 6 6 7 7 7 8 8 8 
+     * </pre>
+     */
+    int box(int r, int c) {
+        return r - r % 3 + c / 3;
+    }
+
+    void solve(int i) {
+        int r = i / SIZE, c = i % SIZE, b = box(r, c);
+        if (r >= SIZE)
+            answer();
+        else if (a[r][c] != 0)
+            solve(i + 1); // 既に番号が付与されている場合は次へ
+        else
+            // r行c列で配置可能な番号について配置を試みる。
+            // vは適用可能な番号のbit値、v ^= bitは処理済のbitをvから除外する。
+            for (int v = MASK & ~(rowSet[r] | colSet[c] | boxSet[b]), bit = 0; v != 0; v ^= bit) {
+                // 適用可能な番号のbitmapから右端(最小)のビットを取り出す。
+                bit = Integer.lowestOneBit(v); // or -v & v
+                set(r, c, bit);     // 配置する。
+                solve(i + 1);       // 次へ進む。
+                unset(r, c, bit);   // もとに戻す。
+            }
+    }
+
+    public static List<int[][]> solve(int[][] a) {
+        Sudoku sudoku = new Sudoku(a);
+        sudoku.solve(0);
+        return sudoku.result;
+    }
+}
--- src/test/java/test/puzzle/core/TestSudoku.java
+++ src/test/java/test/puzzle/core/TestSudoku.java
@@ -11,10 +11,135 @@ import java.util.stream.Stream;
 
 import org.junit.Test;
 
+import puzzle.core.Sudoku;
+
 public class TestSudoku {
 
     static void method() {
 //        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+    }
+
+    @Test
+    public void testSolve() {
+        method();
+        int[][] board = {
+            {7, 0, 2, 0, 5, 0, 6, 0, 0},
+            {0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {1, 0, 0, 0, 0, 9, 5, 0, 0},
+            {8, 0, 0, 0, 0, 0, 0, 9, 0},
+            {0, 4, 3, 0, 0, 0, 7, 5, 0},
+            {0, 9, 0, 0, 0, 0, 0, 0, 8},
+            {0, 0, 9, 7, 0, 0, 0, 0, 5},
+            {0, 0, 0, 2, 0, 0, 0, 0, 0},
+            {0, 0, 7, 0, 4, 0, 2, 0, 3},
+        };
+        int[][] expected = {
+            {7, 3, 2, 4, 5, 8, 6, 1, 9},
+            {9, 5, 6, 1, 7, 3, 8, 2, 4},
+            {1, 8, 4, 6, 2, 9, 5, 3, 7},
+            {8, 7, 1, 5, 6, 4, 3, 9, 2},
+            {6, 4, 3, 8, 9, 2, 7, 5, 1},
+            {2, 9, 5, 3, 1, 7, 4, 6, 8},
+            {3, 2, 9, 7, 8, 6, 1, 4, 5},
+            {4, 1, 8, 2, 3, 5, 9, 7, 6},
+            {5, 6, 7, 9, 4, 1, 2, 8, 3},
+        };
+        List<int[][]> result = Sudoku.solve(board);
+        assertEquals(1, result.size());
+        assertArrayEquals(expected, result.get(0));
+    }
+
+//    @Ignore
+    @Test
+    public void testSolve17() {
+        method();
+        int[][] board = {
+            {0, 0, 0, 8, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 4, 3},
+            {5, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 7, 0, 8, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 2, 0, 0, 3, 0, 0, 0, 0},
+            {6, 0, 0, 0, 0, 0, 0, 7, 5},
+            {0, 0, 3, 4, 0, 0, 0, 0, 0},
+            {0, 0, 0, 2, 0, 0, 6, 0, 0},
+        };
+        int[][] expected = {
+            {2, 3, 7, 8, 4, 1, 5, 6, 9},
+            {1, 8, 6, 7, 9, 5, 2, 4, 3},
+            {5, 9, 4, 3, 2, 6, 7, 1, 8},
+            {3, 1, 5, 6, 7, 4, 8, 9, 2},
+            {4, 6, 9, 5, 8, 2, 1, 3, 7},
+            {7, 2, 8, 1, 3, 9, 4, 5, 6},
+            {6, 4, 2, 9, 1, 8, 3, 7, 5},
+            {8, 5, 3, 4, 6, 7, 9, 2, 1},
+            {9, 7, 1, 2, 5, 3, 6, 8, 4},
+        };
+        List<int[][]> result = Sudoku.solve(board);
+        assertEquals(1, result.size());
+        assertArrayEquals(expected, result.get(0));
+    }
+
+//    @Ignore
+    @Test
+    public void testSolve17_2() {
+        method();
+        int[][] board = {
+            {0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {4, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 5, 0, 4, 0, 7},
+            {0, 0, 8, 0, 0, 0, 3, 0, 0},
+            {0, 0, 1, 0, 9, 0, 0, 0, 0},
+            {3, 0, 0, 4, 0, 0, 2, 0, 0},
+            {0, 5, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 8, 0, 6, 0, 0, 0},
+        };
+        int[][] expected = {
+            {6, 9, 3, 7, 8, 4, 5, 1, 2},
+            {4, 8, 7, 5, 1, 2, 9, 3, 6},
+            {1, 2, 5, 9, 6, 3, 8, 7, 4},
+            {9, 3, 2, 6, 5, 1, 4, 8, 7},
+            {5, 6, 8, 2, 4, 7, 3, 9, 1},
+            {7, 4, 1, 3, 9, 8, 6, 2, 5},
+            {3, 1, 9, 4, 7, 5, 2, 6, 8},
+            {8, 5, 6, 1, 2, 9, 7, 4, 3},
+            {2, 7, 4, 8, 3, 6, 1, 5, 9},
+        };
+        List<int[][]> result = Sudoku.solve(board);
+        assertEquals(1, result.size());
+        assertArrayEquals(expected, result.get(0));
+    }
+
+//    @Ignore
+    @Test
+    public void testSolve17_3() {
+        method();
+        int[][] board = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 2, 3},
+            {0, 0, 4, 0, 0, 5, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 3, 0, 6, 0, 0},
+            {0, 0, 7, 0, 0, 0, 5, 8, 0},
+            {0, 0, 0, 0, 6, 7, 0, 0, 0},
+            {0, 1, 0, 0, 0, 4, 0, 0, 0},
+            {5, 2, 0, 0, 0, 0, 0, 0, 0},
+        };
+        int[][] expected = {
+            {6, 7, 2, 9, 8, 3, 4, 5, 1},
+            {9, 5, 1, 4, 7, 6, 8, 2, 3},
+            {3, 8, 4, 2, 1, 5, 9, 7, 6},
+            {4, 6, 8, 1, 5, 9, 2, 3, 7},
+            {2, 9, 5, 7, 3, 8, 6, 1, 4},
+            {1, 3, 7, 6, 4, 2, 5, 8, 9},
+            {8, 4, 3, 5, 6, 7, 1, 9, 2},
+            {7, 1, 9, 8, 2, 4, 3, 6, 5},
+            {5, 2, 6, 3, 9, 1, 7, 4, 8},
+        };
+        List<int[][]> result = Sudoku.solve(board);
+        assertEquals(1, result.size());
+        assertArrayEquals(expected, result.get(0));
     }
 
     /**
@@ -22,7 +147,7 @@ public class TestSudoku {
      * Java で数独ソルバーを 20 分で作成する - 完全なチュートリアル - YouTube
      * https://www.youtube.com/watch?v=mcXc8Mva2bA&t=1061s
      */
-    static boolean solve(int[][] board) {
+    static boolean solveSimple(int[][] board) {
         int size = 9;
         return new Object() {
 
@@ -79,7 +204,7 @@ public class TestSudoku {
     }
 
     @Test
-    public void testSolve() {
+    public void testSolveSimple() {
         method();
         int[][] board = {
             {7, 0, 2, 0, 5, 0, 6, 0, 0},
@@ -103,7 +228,7 @@ public class TestSudoku {
             {4, 1, 8, 2, 3, 5, 9, 7, 6},
             {5, 6, 7, 9, 4, 1, 2, 8, 3},
         };
-        assertTrue(solve(board));
+        assertTrue(solveSimple(board));
         assertArrayEquals(expected, board);
     }
 
@@ -275,215 +400,6 @@ public class TestSudoku {
             {5, 6, 7, 9, 4, 1, 2, 8, 3},
         };
         List<int[][]> result = solveBitmap(board);
-        assertEquals(1, result.size());
-        assertArrayEquals(expected, result.get(0));
-    }
-
-    /**
-     * bitmapを使って適用可能な番号のみを順次取り出すようにし、
-     * 適用不能な番号のチェックを行わずに済むようにした。
-     * つまり、for (int n = 1; n <= 9; ++n)のループを高速化した。
-     * 終盤になると適用不能な番号が増えてくるので、
-     * そのチェックをスキップできると高速化できる。
-     * xxxSet[]は使用済み番号の集合の配列。
-     * 各要素は番号nが使用済みのとき、2ⁿビットがONになっている。
-     * nは1から9の数字なので、全ての番号が使用済みの時は0b111_111_111_0となる。
-     */
-    static List<int[][]> solveBitmapFast(int[][] a) {
-        List<int[][]> result = new ArrayList<>();
-        int size = 9, mask = 0b111_111_111_0;
-        int[] rowSet = new int[size], colSet = new int[size], boxSet = new int[size];
-        new Object() {
-            {
-                // 配列a初期化：既に確定している番号をbitmapにセットする。
-                for (int r = 0; r < size; ++r)
-                    for (int c = 0; c < size; ++c) {
-                        int n = a[r][c];
-                        if (n != 0)
-                            set(r, c, 1 << n);
-                    }
-            }
-
-            void set(int r, int c, int bit) {
-                rowSet[r] |= bit;
-                colSet[c] |= bit;
-                boxSet[box(r, c)] |= bit;
-                a[r][c] = Integer.numberOfTrailingZeros(bit);
-            }
-
-            void unset(int r, int c, int bit) {
-                rowSet[r] ^= bit;
-                colSet[c] ^= bit;
-                boxSet[box(r, c)] ^= bit;
-                a[r][c] = 0;
-            }
-
-            void answer() {
-                result.add(Stream.of(a)
-                    .map(x -> Arrays.copyOf(x, x.length))
-                    .toArray(int[][]::new));
-            }
-            
-            /**
-             * r行c列が属するbox(3x3)のセル位置を求める。
-             * <pre>
-             * \ c 0 1 2 3 4 5 6 7 8
-             * r +------------------
-             * 0 | 0 0 0 1 1 1 2 2 2 
-             * 1 | 0 0 0 1 1 1 2 2 2 
-             * 2 | 0 0 0 1 1 1 2 2 2 
-             * 3 | 3 3 3 4 4 4 5 5 5 
-             * 4 | 3 3 3 4 4 4 5 5 5 
-             * 5 | 3 3 3 4 4 4 5 5 5 
-             * 6 | 6 6 6 7 7 7 8 8 8 
-             * 7 | 6 6 6 7 7 7 8 8 8 
-             * 8 | 6 6 6 7 7 7 8 8 8 
-             * </pre>
-             */
-            int box(int r, int c) {
-                return r - r % 3 + c / 3;
-            }
-
-            void solve(int i) {
-                int r = i / size, c = i % size, b = box(r, c);
-                if (r >= size)
-                    answer();
-                else if (a[r][c] != 0)
-                    solve(i + 1); // 既に番号が付与されている場合は次へ
-                else
-                    // r行c列で配置可能な番号について配置を試みる。
-                    // vは適用可能な番号のbit値、v ^= bitは処理済のbitをvから除外する。
-                    for (int v = mask & ~(rowSet[r] | colSet[c] | boxSet[b]), bit = 0; v != 0; v ^= bit) {
-                        // 適用可能な番号のbitmapから右端(最小)のビットを取り出す。
-                        bit = Integer.lowestOneBit(v); // or -v & v
-                        set(r, c, bit);     // 配置する。
-                        solve(i + 1);       // 次へ進む。
-                        unset(r, c, bit);   // もとに戻す。
-                    }
-            }
-        }.solve(0);
-        return result;
-    }
-
-    @Test
-    public void testSolveBitmapFast() {
-        method();
-        int[][] board = {
-            {7, 0, 2, 0, 5, 0, 6, 0, 0},
-            {0, 0, 0, 0, 0, 3, 0, 0, 0},
-            {1, 0, 0, 0, 0, 9, 5, 0, 0},
-            {8, 0, 0, 0, 0, 0, 0, 9, 0},
-            {0, 4, 3, 0, 0, 0, 7, 5, 0},
-            {0, 9, 0, 0, 0, 0, 0, 0, 8},
-            {0, 0, 9, 7, 0, 0, 0, 0, 5},
-            {0, 0, 0, 2, 0, 0, 0, 0, 0},
-            {0, 0, 7, 0, 4, 0, 2, 0, 3},
-        };
-        int[][] expected = {
-            {7, 3, 2, 4, 5, 8, 6, 1, 9},
-            {9, 5, 6, 1, 7, 3, 8, 2, 4},
-            {1, 8, 4, 6, 2, 9, 5, 3, 7},
-            {8, 7, 1, 5, 6, 4, 3, 9, 2},
-            {6, 4, 3, 8, 9, 2, 7, 5, 1},
-            {2, 9, 5, 3, 1, 7, 4, 6, 8},
-            {3, 2, 9, 7, 8, 6, 1, 4, 5},
-            {4, 1, 8, 2, 3, 5, 9, 7, 6},
-            {5, 6, 7, 9, 4, 1, 2, 8, 3},
-        };
-        List<int[][]> result = solveBitmapFast(board);
-        assertEquals(1, result.size());
-        assertArrayEquals(expected, result.get(0));
-    }
-
-//    @Ignore
-    @Test
-    public void testSolveBitmapFast17() {
-        method();
-        int[][] board = {
-            {0, 0, 0, 8, 0, 1, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 4, 3},
-            {5, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 7, 0, 8, 0, 0},
-            {0, 0, 0, 0, 0, 0, 1, 0, 0},
-            {0, 2, 0, 0, 3, 0, 0, 0, 0},
-            {6, 0, 0, 0, 0, 0, 0, 7, 5},
-            {0, 0, 3, 4, 0, 0, 0, 0, 0},
-            {0, 0, 0, 2, 0, 0, 6, 0, 0},
-        };
-        int[][] expected = {
-            {2, 3, 7, 8, 4, 1, 5, 6, 9},
-            {1, 8, 6, 7, 9, 5, 2, 4, 3},
-            {5, 9, 4, 3, 2, 6, 7, 1, 8},
-            {3, 1, 5, 6, 7, 4, 8, 9, 2},
-            {4, 6, 9, 5, 8, 2, 1, 3, 7},
-            {7, 2, 8, 1, 3, 9, 4, 5, 6},
-            {6, 4, 2, 9, 1, 8, 3, 7, 5},
-            {8, 5, 3, 4, 6, 7, 9, 2, 1},
-            {9, 7, 1, 2, 5, 3, 6, 8, 4},
-        };
-        List<int[][]> result = solveBitmapFast(board);
-        assertEquals(1, result.size());
-        assertArrayEquals(expected, result.get(0));
-    }
-
-//    @Ignore
-    @Test
-    public void testSolveBitmapFast17_2() {
-        method();
-        int[][] board = {
-            {0, 0, 0, 0, 0, 0, 0, 1, 0},
-            {4, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 2, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 5, 0, 4, 0, 7},
-            {0, 0, 8, 0, 0, 0, 3, 0, 0},
-            {0, 0, 1, 0, 9, 0, 0, 0, 0},
-            {3, 0, 0, 4, 0, 0, 2, 0, 0},
-            {0, 5, 0, 1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 8, 0, 6, 0, 0, 0},
-        };
-        int[][] expected = {
-            {6, 9, 3, 7, 8, 4, 5, 1, 2},
-            {4, 8, 7, 5, 1, 2, 9, 3, 6},
-            {1, 2, 5, 9, 6, 3, 8, 7, 4},
-            {9, 3, 2, 6, 5, 1, 4, 8, 7},
-            {5, 6, 8, 2, 4, 7, 3, 9, 1},
-            {7, 4, 1, 3, 9, 8, 6, 2, 5},
-            {3, 1, 9, 4, 7, 5, 2, 6, 8},
-            {8, 5, 6, 1, 2, 9, 7, 4, 3},
-            {2, 7, 4, 8, 3, 6, 1, 5, 9},
-        };
-        List<int[][]> result = solveBitmapFast(board);
-        assertEquals(1, result.size());
-        assertArrayEquals(expected, result.get(0));
-    }
-
-//    @Ignore
-    @Test
-    public void testSolveBitmapFast17_3() {
-        method();
-        int[][] board = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {0, 0, 0, 0, 0, 0, 0, 2, 3},
-            {0, 0, 4, 0, 0, 5, 0, 0, 0},
-            {0, 0, 0, 1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 3, 0, 6, 0, 0},
-            {0, 0, 7, 0, 0, 0, 5, 8, 0},
-            {0, 0, 0, 0, 6, 7, 0, 0, 0},
-            {0, 1, 0, 0, 0, 4, 0, 0, 0},
-            {5, 2, 0, 0, 0, 0, 0, 0, 0},
-        };
-        int[][] expected = {
-            {6, 7, 2, 9, 8, 3, 4, 5, 1},
-            {9, 5, 1, 4, 7, 6, 8, 2, 3},
-            {3, 8, 4, 2, 1, 5, 9, 7, 6},
-            {4, 6, 8, 1, 5, 9, 2, 3, 7},
-            {2, 9, 5, 7, 3, 8, 6, 1, 4},
-            {1, 3, 7, 6, 4, 2, 5, 8, 9},
-            {8, 4, 3, 5, 6, 7, 1, 9, 2},
-            {7, 1, 9, 8, 2, 4, 3, 6, 5},
-            {5, 2, 6, 3, 9, 1, 7, 4, 8},
-        };
-        List<int[][]> result = solveBitmapFast(board);
         assertEquals(1, result.size());
         assertArrayEquals(expected, result.get(0));
     }

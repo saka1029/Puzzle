d773205c8733a5dd7eb6ea3e6e4ed330e092ef78
saka1029 <saka1029@gmail.com>
2026-02-13 07:54:27 +0900

    ..

---
+++ src/test/java/test/puzzle/core/TestKomachi2.java
@@ -0,0 +1,55 @@
+package test.puzzle.core;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import org.junit.Test;
+
+public class TestKomachi2 {
+
+    enum Operator {
+        Append(-1) { public int eval(int l, int r) { throw new RuntimeException("Cant eval");}},
+        Minus(0) { public int eval(int l, int r) { return l - r;}},
+        Plus(0) { public int eval(int l, int r) { return l + r;}},
+        Divide(1) { public int eval(int l, int r) { return l / r;}},
+        Multiply(1) { public int eval(int l, int r) { return l * r;}};
+
+        static final Operator[] all = Operator.values();
+        public final int precedance;
+
+        Operator(int precedance) {
+            this.precedance = precedance;
+        }
+
+        public static Operator at(int index) {
+            return all[index];
+        }
+
+        public abstract int eval(int left, int right);
+    }
+
+    static class Calcurator {
+        final Deque<Integer> stack = new ArrayDeque<>();
+        final Deque<Operator> opStack = new ArrayDeque<>();
+
+        public void add(Operator op, int digit) {
+            switch (op) {
+                case Append: stack.push(stack.pop() * 10 + digit); break;
+                default:
+                    opStack.push(op);
+                    stack.push(digit);
+                    break;
+            }
+
+        }
+    }
+
+    @Test
+    public void testOperator() {
+        assertEquals(Operator.Append, Operator.at(0));
+        assertEquals(Operator.Multiply, Operator.at(4));
+    }
+
+}

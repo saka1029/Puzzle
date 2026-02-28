c8f4998f2c26f356cebc1ff9c50b4792e9fc9ed8
saka1029 <saka1029@gmail.com>
2026-03-01 05:16:59 +0900

    ..

--- src/test/java/test/puzzle/core/TestKomachiCentury.java
+++ src/test/java/test/puzzle/core/TestKomachiCentury.java
@@ -16,13 +16,15 @@ public class TestKomachiCentury {
         Rational goal = Rational.of(100);
         int[] digits = IntStream.rangeClosed(1, 9).toArray();
         for (int[] array : Permutation.iterable(digits, 9)) {
-            for (int ae = 0; ae <= 3; ++ae) {
+            for (int ae = 0; ae <= 2; ++ae) {
                 int a = subDigits(array, 0, ae);
-                for (int be = ae + 1; be < 8; ++be) {
+                for (int be = ae + 1; be <= 8; ++be) {
                     int b = subDigits(array, ae, be);
                     int c = subDigits(array, be, 9);
                     if (Rational.of(a).add(Rational.of(b, c)).equals(goal))
                         System.out.printf("%d + %d/%d == %s%n",a, b, c, goal);
+                    // if (Rational.of(a).subtract(Rational.of(b, c)).equals(goal))
+                    //     System.out.printf("%d - %d/%d == %s%n",a, b, c, goal);
                 }
             }
         }

d0ba13d8732da71be01d3960b3a3bb0e4a295d85
saka1029 <saka1029@gmail.com>
2026-08-07 07:11:20 +0900

    ..

--- src/test/java/test/puzzle/matrix/TestMatrix.java
+++ src/test/java/test/puzzle/matrix/TestMatrix.java
@@ -10,6 +10,7 @@ public class TestMatrix {
 
     public static class Matrix<T> {
         public final int[] dimensions;
+        public final int[] encode;
         public final T[] array;
 
         @SuppressWarnings("unchecked")
@@ -22,13 +23,56 @@ public class TestMatrix {
             this.dimensions = dimensions;
             int size = IntStream.of(dimensions).reduce(1, (a, b) -> a * b);
             this.array = (T[])Array.newInstance(componentType, size);
+            int i = dimensions.length;
+            this.encode = new int[i];
+            encode[--i] = 1;
+            int prev = encode[i];
+            for ( ; i >= 0; --i) {
+                encode[i] = prev;
+                prev *= dimensions[i];
+            }
+        }
+
+        int index(int... index) {
+            return IntStream.range(0, dimensions.length)
+                .map(i -> index[i] * encode[i])
+                .sum();
+        }
+
+        void checkIndex(int... index) {
+            if (index.length != dimensions.length)
+                throw new IndexOutOfBoundsException(
+                    "'index' length must be %d but %d".formatted(dimensions.length, index.length));
+            for (int i = 0, len = dimensions.length; i < len; ++i)
+                if (index[i] < 0 || index[i] > dimensions[i])
+                    throw new IndexOutOfBoundsException(
+                        "index must be in range 0..<%d but %d".formatted(dimensions[i], index[i]));
+        }
+
+        public T get(int... index) {
+             return array[index(index)];
+        }
+
+        public void set(T value, int... index) {
+            checkIndex(index);
+            array[index(index)] = value;
         }
     }
 
     @Test
     public void testMatrix() {
-        Matrix<Double> m = new Matrix<>(new int[]{2, 3});
+        Matrix<Double> m = new Matrix<>(new int[]{2, 3, 4});
+        double v = 0;
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 3; ++j)
+                for (int k = 0; k < 4; ++k)
+                    m.set(v++, i, j, k);
         System.out.println("dimensions=" + Arrays.toString(m.dimensions));
         System.out.println("array=" + Arrays.toString(m.array));
+        System.out.println("encode=" + Arrays.toString(m.encode));
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 3; ++j)
+                for (int k = 0; k < 4; ++k)
+                    System.out.printf(" %s", m.get(i, j, k));
     }
 }
